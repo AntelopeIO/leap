f5c59c943d28a95240121bf78312b685eb9d754e
Henry Monti <henry.monti@block.one>
2021-08-25 16:17:08 -0400

    Reworked test to check lib and head with node.getInfo(), it no longer uses cluster.getBlockLog(). Modified Node.py to accomodate the new test.

--- tests/Node.py
+++ tests/Node.py
@@ -1092,7 +1092,7 @@ class Node(object):
 
     # pylint: disable=too-many-locals
     # If nodeosPath is equal to None, it will use the existing nodeos path
-    def relaunch(self, chainArg=None, newChain=False, skipGenesis=True, timeout=Utils.systemWaitTimeout, addSwapFlags=None, cachePopen=False, nodeosPath=None, termMaxBlock=10):
+    def relaunch(self, chainArg=None, newChain=False, skipGenesis=True, timeout=Utils.systemWaitTimeout, addSwapFlags=None, cachePopen=False, nodeosPath=None, waitForTerm=True):
 
         assert(self.pid is None)
         assert(self.killed)
@@ -1147,16 +1147,12 @@ class Node(object):
             except TimeoutExpired:
                 return False
             with open(popen.errfile.name, 'r') as f:
-                termSuccessMsg = (
-                    "Reached configured maximum block {}; terminating"
-                ).format(termMaxBlock)
-
-                if termSuccessMsg in f.read():
+                if "Reached configured maximum block 10; terminating" in f.read():
                     return True
                 else:
                     return False
 
-        if "terminate-at-block" not in cmd:
+        if "terminate-at-block" not in cmd or not waitForTerm:
             isAlive=Utils.waitForBool(isNodeAlive, timeout, sleepTime=1)
         else:
             isAlive=Utils.waitForBoolWithArg(didNodeExitGracefully, self.popenProc, timeout, sleepTime=1)
