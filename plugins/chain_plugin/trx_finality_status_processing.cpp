84fde0882b2d1410658cd1931d8afd579fae89aa
Brian Johnson <johnsonb@objectcomputing.com>
2022-03-28 06:58:50 -0700

    Cleanup of unused code.

--- plugins/chain_plugin/trx_finality_status_processing.cpp
+++ plugins/chain_plugin/trx_finality_status_processing.cpp
@@ -9,23 +9,4 @@ namespace eosio::chain_apis {
    {
    }
 
-   chain::block_interface_ptr trx_finality_status_processing::get_block_processor() const {
-#warning TODO need to add real block_interface implementation
-      return chain::block_interface_ptr{};
-   }
-
-   chain::trx_interface_ptr trx_finality_status_processing::get_in_block_trx_processor() const {
-#warning TODO need to add real trx_interface implementation
-      return chain::trx_interface_ptr{};
-   }
-
-   chain::trx_interface_ptr trx_finality_status_processing::get_speculative_trx_processor() const {
-      // trx finality status does not care about speculative block transactions
-      return chain::trx_interface_ptr{new chain::no_op_processor};
-   }
-
-   chain::trx_interface_ptr trx_finality_status_processing::get_local_trx_processor() const {
-#warning TODO need to add real trx_interface implementation
-      return chain::trx_interface_ptr{};
-   }
 }
