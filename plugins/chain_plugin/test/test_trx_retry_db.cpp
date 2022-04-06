cd78da79d65f919fd6d1e0f6bd6b2eecc807c3c3
Kevin Heifner <heifnerk@objectcomputing.com>
2022-04-06 12:12:04 -0500

    Add forking unittest

--- plugins/chain_plugin/test/test_trx_retry_db.cpp
+++ plugins/chain_plugin/test/test_trx_retry_db.cpp
@@ -416,7 +416,164 @@ BOOST_AUTO_TEST_CASE(trx_retry_logic) {
       //
       // test forking
       //
+      auto trx_7 = make_unique_trx(chain->get_chain_id(), fc::seconds(30), 7);
+      bool trx_7_variant = false;
+      trx_retry.track_transaction( trx_7, lib, [&trx_7_variant](const std::variant<fc::exception_ptr, std::unique_ptr<fc::variant>>& result){
+         BOOST_REQUIRE( std::holds_alternative<std::unique_ptr<fc::variant>>(result) );
+         BOOST_CHECK( !!std::get<std::unique_ptr<fc::variant>>(result) );
+         trx_7_variant = true;
+      } );
+      // increase time by 1 seconds, so trx_8 retry interval diff than 7
+      pnow += boost::posix_time::seconds(1);
+      fc::mock_time_traits::set_now(pnow);
+      auto trx_8 = make_unique_trx(chain->get_chain_id(), fc::seconds(30), 8);
+      bool trx_8_variant = false;
+      trx_retry.track_transaction( trx_8, std::optional<uint32_t>(3), [&trx_8_variant](const std::variant<fc::exception_ptr, std::unique_ptr<fc::variant>>& result){
+         BOOST_REQUIRE( std::holds_alternative<std::unique_ptr<fc::variant>>(result) );
+         BOOST_CHECK( !!std::get<std::unique_ptr<fc::variant>>(result) );
+         trx_8_variant = true;
+      } );
+      // one to expire, will be forked out never to return
+      auto trx_9 = make_unique_trx(chain->get_chain_id(), fc::seconds(30), 9);
+      bool trx_9_expired = false;
+      trx_retry.track_transaction( trx_9, lib, [&trx_9_expired](const std::variant<fc::exception_ptr, std::unique_ptr<fc::variant>>& result){
+         BOOST_REQUIRE( std::holds_alternative<fc::exception_ptr>(result) );
+         BOOST_CHECK_EQUAL( std::get<fc::exception_ptr>(result)->code(), expired_tx_exception::code_value );
+         trx_9_expired = true;
+      } );
 
+      // not in block 12
+      auto bsp12 = make_block_state(12, {});
+      trx_retry.on_block_start(12);
+      trx_retry.on_accepted_block(bsp12);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(!trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      // 7,8 in block 13
+      pnow += boost::posix_time::seconds(1); // new block, new time
+      fc::mock_time_traits::set_now(pnow);
+      trx_retry.on_block_start(13);
+      auto trace_7 = make_transaction_trace( trx_7, 13);
+      auto trace_8 = make_transaction_trace( trx_8, 13);
+      auto trace_9 = make_transaction_trace( trx_9, 13);
+      trx_retry.on_applied_transaction(trace_7, trx_7);
+      trx_retry.on_applied_transaction(trace_8, trx_8);
+      trx_retry.on_applied_transaction(trace_9, trx_9);
+      auto bsp13 = make_block_state(13, {trx_7, trx_8, trx_9});
+      trx_retry.on_accepted_block(bsp13);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(!trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      // need 3 blocks before 8 returned to user
+      pnow += boost::posix_time::seconds(1); // new block, new time, 1st block
+      fc::mock_time_traits::set_now(pnow);
+      auto bsp14 = make_block_state(14, {});
+      trx_retry.on_block_start(14);
+      trx_retry.on_accepted_block(bsp14);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(!trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      pnow += boost::posix_time::seconds(1); // new block, new time, 2nd block
+      fc::mock_time_traits::set_now(pnow);
+      auto bsp15 = make_block_state(15, {});
+      trx_retry.on_block_start(15);
+      trx_retry.on_accepted_block(bsp15);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(!trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      // now fork out including 13 which had traces, trx_9 will be forked out and not re-appear
+      pnow += boost::posix_time::seconds(1); // new block, new time
+      fc::mock_time_traits::set_now(pnow);
+      trx_retry.on_block_start(13);
+      // should still be tracking them
+      BOOST_CHECK_EQUAL(3, trx_retry.size());
+      // now produce an empty 13
+      auto bsp13b = make_block_state(13, {}); // now 13 has no traces
+      trx_retry.on_accepted_block(bsp13b);
+      // produced another empty block
+      pnow += boost::posix_time::seconds(1); // new block, new time
+      fc::mock_time_traits::set_now(pnow);
+      trx_retry.on_block_start(14);
+      // now produce an empty 14
+      auto bsp14b = make_block_state(14, {}); // empty
+      trx_retry.on_accepted_block(bsp14b);
+      // produce block with 7,8
+      trx_retry.on_block_start(15);
+      auto trace_7b = make_transaction_trace( trx_7, 15);
+      auto trace_8b = make_transaction_trace( trx_8, 15);
+      trx_retry.on_applied_transaction(trace_7b, trx_7);
+      trx_retry.on_applied_transaction(trace_8b, trx_8);
+      auto bsp15b = make_block_state(15, {trx_7, trx_8});
+      trx_retry.on_accepted_block(bsp15b);
+      // need 3 blocks before 8 returned to user
+      pnow += boost::posix_time::seconds(1); // new block, new time
+      fc::mock_time_traits::set_now(pnow);
+      auto bsp16 = make_block_state(16, {});
+      trx_retry.on_block_start(16);
+      trx_retry.on_accepted_block(bsp16);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(!trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      pnow += boost::posix_time::seconds(1); // new block, new time
+      fc::mock_time_traits::set_now(pnow);
+      auto bsp17 = make_block_state(17, {});
+      trx_retry.on_block_start(17);
+      trx_retry.on_accepted_block(bsp17);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(!trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      pnow += boost::posix_time::seconds(1); // new block, new time, 3rd one
+      fc::mock_time_traits::set_now(pnow);
+      auto bsp18 = make_block_state(18, {});
+      trx_retry.on_block_start(18);
+      trx_retry.on_accepted_block(bsp18);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      trx_retry.on_irreversible_block(bsp9);
+      trx_retry.on_irreversible_block(bsp10);
+      trx_retry.on_irreversible_block(bsp11);
+      trx_retry.on_irreversible_block(bsp12);
+      trx_retry.on_irreversible_block(bsp13b);
+      trx_retry.on_irreversible_block(bsp14b);
+      BOOST_CHECK(!trx_7_variant);
+      BOOST_CHECK(trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      trx_retry.on_irreversible_block(bsp15b);
+      BOOST_CHECK(trx_7_variant);
+      BOOST_CHECK(trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      // verify trx_9 expires
+      pnow += boost::posix_time::seconds(21); // new block, new time, before expire
+      fc::mock_time_traits::set_now(pnow);
+      auto bsp19 = make_block_state(19, {});
+      trx_retry.on_block_start(19);
+      trx_retry.on_accepted_block(bsp19);
+      trx_retry.on_irreversible_block(bsp15);
+      trx_retry.on_irreversible_block(bsp16);
+      trx_retry.on_irreversible_block(bsp17);
+      trx_retry.on_irreversible_block(bsp18);
+      trx_retry.on_irreversible_block(bsp19);
+      BOOST_CHECK(trx_7_variant);
+      BOOST_CHECK(trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      pnow += boost::posix_time::seconds(1); // new block, new time, trx_9 now expired
+      fc::mock_time_traits::set_now(pnow);
+      auto bsp20 = make_block_state(20, {});
+      trx_retry.on_block_start(20);
+      trx_retry.on_accepted_block(bsp20);
+      // waits for LIB
+      BOOST_CHECK(trx_7_variant);
+      BOOST_CHECK(trx_8_variant);
+      BOOST_CHECK(!trx_9_expired);
+      trx_retry.on_irreversible_block(bsp20);
+      BOOST_CHECK(trx_7_variant);
+      BOOST_CHECK(trx_8_variant);
+      BOOST_CHECK(trx_9_expired);
+      BOOST_CHECK_EQUAL(0, trx_retry.size());
+
+
+      // shutdown
       appbase::app().quit();
       app_thread.join();
 
