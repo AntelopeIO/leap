2cf86b41eba62ed8208e9dcd173339182ee0f47b
Clayton Calabrese <calabresec@objectcomputing.com>
2022-04-08 18:24:56 -0500

    change expired_accumulator_average_window to member variable of subjective_billing.hpp

--- plugins/producer_plugin/subjective_billing.cpp
+++
@@ -1,7 +0,0 @@
-#include <eosio/producer_plugin/subjective_billing.hpp>
-
-namespace eosio {
-
-uint32_t subjective_billing::expired_accumulator_average_window = config::account_cpu_usage_average_window_ms / subjective_billing::subjective_time_interval_ms;
-
-} //eosio
