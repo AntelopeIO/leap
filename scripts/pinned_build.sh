90ebab686ce402ab802a2e7de1234e70abebc4af
Larry Kittinger <bucky@eosn.foundation>
2022-05-11 05:46:50 -0400

    remove a lot

--- scripts/pinned_build.sh
+++ scripts/pinned_build.sh
@@ -25,60 +25,6 @@ LIBPQXX_VER=7.2.1
 pushd $DEP_DIR &> /dev/null
 mkdir -p $DEP_DIR
 
-# install dependencies
-
-# download boost and build boost
-BOOST_DIR=${DEP_DIR}/boost_${BOOST_VER//\./_}
-
-if [ ! -d "${BOOST_DIR}" ]; then
-   echo "Installing Boost ${BOOST_VER} @ ${BOOST_DIR}"
-   wget https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VER}/source/boost_${BOOST_VER//\./_}.tar.gz
-   tar -xvzf boost_${BOOST_VER//\./_}.tar.gz -C ${DEP_DIR}
-   pushd ${BOOST_DIR} &> /dev/null
-   ./bootstrap.sh --prefix=${BOOST_DIR}/bin
-   ./b2 cxxflags="-stdlib=libc++ -D__STRICT_ANSI__ -nostdinc++ -I${CLANG_DIR}/include/c++/v1" linkflags='-stdlib=libc++' link=static threading=multi --with-iostreams --with-date_time --with-filesystem --with-system --with-program_options --with-chrono --with-test -q -j${JOBS} install
-   popd &> /dev/null
-   rm boost_${BOOST_VER//\./_}.tar.gz
-fi
-
-# download LLVM and install
-LLVM_DIR=${DEP_DIR}/llvm-${LLVM_VER}
-
-if [ ! -d "${LLVM_DIR}" ]; then
-   echo "Installing LLVM ${LLVM_VER} @ ${LLVM_DIR}"
-   mkdir -p ${LLVM_DIR}
-   wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${LLVM_VER}/llvm-${LLVM_VER}.src.tar.xz
-   tar -xvf llvm-${LLVM_VER}.src.tar.xz
-   pushd "${LLVM_DIR}.src"
-   mkdir build
-   pushd build &> /dev/null
-   cmake -DCMAKE_INSTALL_PREFIX=${LLVM_DIR} -DLLVM_TARGETS_TO_BUILD=host -DLLVM_BUILD_TOOLS=false -DLLVM_ENABLE_RTTI=1 ..
-   make -j${JOBS} && make -j${JOBS} install
-   popd &> /dev/null
-   rm -rf ${LLVM_DIR}.src 
-   rm llvm-${LLVM_VER}.src.tar.xz
-fi
-
-# download libpqxx
-LIBPQXX_DIR=${DEP_DIR}/libpqxx-${LIBPQXX_VER}
-
-if [ ! -d "${LIBPQXX_DIR}" ]; then
-   echo "Installing libpqxx ${LIBPQXX_VER} @ ${LIBPQXX_DIR}"
-   mkdir -p ${LIBPQXX_DIR}
-   wget https://github.com/jtv/libpqxx/archive/${LIBPQXX_VER}.tar.gz
-   tar -xvzf ${LIBPQXX_VER}.tar.gz
-   pushd ${LIBPQXX_DIR} &> /dev/null
-   mkdir -p build
-   pushd build &> /dev/null
-   cmake  -DCMAKE_INSTALL_PREFIX=${LIBPQXX_DIR} -DSKIP_BUILD_TEST=On -DCMAKE_BUILD_TYPE=Release -DPostgreSQL_TYPE_INCLUDE_DIR=/usr/include/postgresql ..
-   make -j${JOBS} && make -j${JOBS} install
-   popd &> /dev/null
-   popd &> /dev/null
-   rm ${LIBPQXX_VER}.tar.gz
-fi
-
-popd
-
 # build Mandel
 MANDEL_DIR=${DEP_DIR}/mandel
 
