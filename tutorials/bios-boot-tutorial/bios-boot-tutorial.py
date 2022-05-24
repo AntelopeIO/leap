72cc02a7dbeadc4ac8a78eb180f3a86a09fce997
Clayton Calabrese <calabresec@objectcomputing.com>
2022-05-23 20:07:21 -0500

    update feature hashes for tutorial

--- tutorials/bios-boot-tutorial/bios-boot-tutorial.py
+++ tutorials/bios-boot-tutorial/bios-boot-tutorial.py
@@ -316,12 +316,10 @@ def stepSetSystemContract():
     sleep(3)
 
     # activate remaining features
-    # KV_DATABASE
-    retry(args.cleos + 'push action eosio activate \'["825ee6288fb1373eab1b5187ec2f04f6eacb39cb3a97f356a07c91622dd61d16"]\' -p eosio@active')
     # ACTION_RETURN_VALUE
     retry(args.cleos + 'push action eosio activate \'["c3a6138c5061cf291310887c0b5c71fcaffeab90d5deb50d3b9e687cead45071"]\' -p eosio@active')
-    # CONFIGURABLE_WASM_LIMITS
-    retry(args.cleos + 'push action eosio activate \'["bf61537fd21c61a60e542a5d66c3f6a78da0589336868307f94a82bccea84e88"]\' -p eosio@active')
+    # CONFIGURABLE_WASM_LIMITS2
+    retry(args.cleos + 'push action eosio activate \'["218036d611869ca2a3f66f96003ad652d80a8eda847a74af45e70c265fe5558c"]\' -p eosio@active')
     # BLOCKCHAIN_PARAMETERS
     retry(args.cleos + 'push action eosio activate \'["5443fcf88330c586bc0e5f3dee10e7f63c76c00249c87fe4fbf7f38c082006b4"]\' -p eosio@active')
     # GET_SENDER
