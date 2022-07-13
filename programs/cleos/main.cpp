6236f47996511bf348eaa85871f105a8a139a4b1
Lin Huang <lin.huang@eosnetwork.com>
2022-07-12 20:39:23 -0400

    update protocol feature list to match Mandel, clean up help, and remove PREACTIVATE_FEATURE as it cannot be activated via activate action

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -2253,16 +2253,32 @@ struct activate_subcommand {
    string feature_name_str;
 
    activate_subcommand(CLI::App* actionRoot) {
-      auto activate = actionRoot->add_subcommand("activate", localized("Activate system feature by feature name eg: KV_DATABASE"));
-      activate->add_option("feature",  feature_name_str,  localized("The system feature name to be activated, must be one of below(lowercase also works):\nPREACTIVATE_FEATURE\nONLY_LINK_TO_EXISTING_PERMISSION\nFORWARD_SETCODE\nKV_DATABASE\nWTMSIG_BLOCK_SIGNATURES\nREPLACE_DEFERRED\nNO_DUPLICATE_DEFERRED_ID\nRAM_RESTRICTIONS\nWEBAUTHN_KEY\nBLOCKCHAIN_PARAMETERS\nDISALLOW_EMPTY_PRODUCER_SCHEDULE\nONLY_BILL_FIRST_AUTHORIZER\nRESTRICT_ACTION_TO_SELF\nCONFIGURABLE_WASM_LIMITS\nACTION_RETURN_VALUE\nFIX_LINKAUTH_RESTRICTION\nGET_SENDER"))->required();
+      auto activate = actionRoot->add_subcommand("activate", localized("Activate protocol feature by name"));
+      activate->add_option("feature",  feature_name_str,  localized("The name, one of below (lowercase also works):\n"
+                       "ONLY_LINK_TO_EXISTING_PERMISSION\n"
+                       "FORWARD_SETCODE\n"
+                       "WTMSIG_BLOCK_SIGNATURES\n"
+                       "CONFIGURABLE_WASM_LIMITS2\n"
+                       "REPLACE_DEFERRED\n"
+                       "NO_DUPLICATE_DEFERRED_ID\n"
+                       "RAM_RESTRICTIONS\n"
+                       "WEBAUTHN_KEY\n"
+                       "DISALLOW_EMPTY_PRODUCER_SCHEDULE\n"
+                       "ONLY_BILL_FIRST_AUTHORIZER\n"
+                       "BLOCKCHAIN_PARAMETERS\n"
+                       "GET_CODE_HASH\n"
+                       "RESTRICT_ACTION_TO_SELF\n"
+                       "ACTION_RETURN_VALUE\n"
+                       "FIX_LINKAUTH_RESTRICTION\n"
+                       "GET_SENDER\n"
+                       "CRYPTO_PRIMITIVES\n"
+                       "GET_BLOCK_NUM"))->required();
       activate->fallthrough(false);
       activate->callback([this] {
          /// map feature name to feature digest
          std::unordered_map<std::string, std::string> map_name_digest{
-            {"PREACTIVATE_FEATURE",              "0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd"},
             {"ONLY_LINK_TO_EXISTING_PERMISSION", "1a99a59d87e06e09ec5b028a9cbb7749b4a5ad8819004365d02dc4379a8b7241"},
             {"FORWARD_SETCODE",                  "2652f5f96006294109b3dd0bbde63693f55324af452b799ee137a81a905eed25"},
-            {"KV_DATABASE",                      "825ee6288fb1373eab1b5187ec2f04f6eacb39cb3a97f356a07c91622dd61d16"},
             {"WTMSIG_BLOCK_SIGNATURES",          "299dcb6af692324b899b39f16d5a530a33062804e41f09dc97e9f156b4476707"},
             {"REPLACE_DEFERRED",                 "ef43112c6543b88db2283a2e077278c315ae2c84719a8b25f25cc88565fbea99"},
             {"NO_DUPLICATE_DEFERRED_ID",         "4a90c00d55454dc5b059055ca213579c6ea856967712a56017487886a4d4cc0f"},
@@ -2271,11 +2287,14 @@ struct activate_subcommand {
             {"BLOCKCHAIN_PARAMETERS",            "5443fcf88330c586bc0e5f3dee10e7f63c76c00249c87fe4fbf7f38c082006b4"},
             {"DISALLOW_EMPTY_PRODUCER_SCHEDULE", "68dcaa34c0517d19666e6b33add67351d8c5f69e999ca1e37931bc410a297428"},
             {"ONLY_BILL_FIRST_AUTHORIZER",       "8ba52fe7a3956c5cd3a656a3174b931d3bb2abb45578befc59f283ecd816a405"},
+            {"GET_CODE_HASH",                    "bcd2a26394b36614fd4894241d3c451ab0f6fd110958c3423073621a70826e99"},
             {"RESTRICT_ACTION_TO_SELF",          "ad9e3d8f650687709fd68f4b90b41f7d825a365b02c23a636cef88ac2ac00c43"},
-            {"CONFIGURABLE_WASM_LIMITS",         "bf61537fd21c61a60e542a5d66c3f6a78da0589336868307f94a82bccea84e88"},
+            {"CONFIGURABLE_WASM_LIMITS2",        "d528b9f6e9693f45ed277af93474fd473ce7d831dae2180cca35d907bd10cb40"},
             {"ACTION_RETURN_VALUE",              "c3a6138c5061cf291310887c0b5c71fcaffeab90d5deb50d3b9e687cead45071"},
             {"FIX_LINKAUTH_RESTRICTION",         "e0fb64b1085cc5538970158d05a009c24e276fb94e1a0bf6a528b48fbc4ff526"},
-            {"GET_SENDER",                       "f0af56d2c5a48d60a4a5b5c903edfb7db3a736a94ed589d0b797df33ff9d3e1d"}
+            {"GET_SENDER",                       "f0af56d2c5a48d60a4a5b5c903edfb7db3a736a94ed589d0b797df33ff9d3e1d"},
+            {"CRYPTO_PRIMITIVES",                "6bcb40a24e49c26d0a60513b6aeb8551d264e4717f306b81a37a5afb3b47cedc"},
+            {"GET_BLOCK_NUM",                    "35c2186cc36f7bb4aeaf4487b36e57039ccf45a9136aa856a5d569ecca55ef2b"}
          };
          // push system feature
          string contract_account = "eosio";
