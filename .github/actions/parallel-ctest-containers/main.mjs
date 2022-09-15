e9d99f71f589782ac4a43f0652c4f393df98a66f
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2022-09-15 14:41:25 -0400

    move NP&LR test population into action; fix action zstd on u18

--- .github/actions/parallel-ctest-containers/main.mjs
+++ .github/actions/parallel-ctest-containers/main.mjs
@@ -9,15 +9,21 @@ import core from '@actions/core'
 const container = core.getInput('container', {required: true});
 const error_log_paths = JSON.parse(core.getInput('error-log-paths', {required: true}));
 const log_tarball_prefix = core.getInput('log-tarball-prefix', {required: true});
-const tests = JSON.parse(core.getInput('tests', {required: true}));
+const tests_label = core.getInput('tests-label', {required: true});
 
 try {
-   if(child_process.spawnSync("docker", ["run", "--name", "base", "-v", `${process.cwd()}/build.tar.zst:/build.tar.zst`, "--workdir", "/__w/leap/leap", container, "tar",  "--zstd", "-xf", "/build.tar.zst"], {stdio:"inherit"}).status)
+   if(child_process.spawnSync("docker", ["run", "--name", "base", "-v", `${process.cwd()}/build.tar.zst:/build.tar.zst`, "--workdir", "/__w/leap/leap", container, "sh", "-c", "zstdcat /build.tar.zst | tar x"], {stdio:"inherit"}).status)
       throw new Error("Failed to create base container");
    if(child_process.spawnSync("docker", ["commit", "base", "baseimage"], {stdio:"inherit"}).status)
       throw new Error("Failed to create base image");
    if(child_process.spawnSync("docker", ["rm", "base"], {stdio:"inherit"}).status)
       throw new Error("Failed to remove base container");
+
+   // the correct approach is by far "--show-only=json-v1" and then pluck out .tests[].name; but that doesn't work on U18's cmake 3.10 since it lacks json-v1 output
+   const test_query_result = child_process.spawnSync("docker", ["run", "--rm", "baseimage", "bash", "-e", "-o", "pipefail", "-c", `cd build; ctest -L '${tests_label}' --show-only | head -n -1 | cut -d ':' -f 2 -s | jq -cnR '[inputs | select(length>0)[1:]]'`]);
+   if(test_query_result.status)
+      throw new Error("Failed to discover tests with label")
+   const tests = JSON.parse(test_query_result.stdout);
 
    let subprocesses = [];
    tests.forEach(t => {
