c5b07a3b1851387ab5715e3386010134f81c859c
Clayton Calabrese <calabresec@objectcomputing.com>
2022-09-29 14:52:40 -0500

    allow node specific customization of logging levels via cluster

--- tests/TestHarness/Cluster.py
+++ tests/TestHarness/Cluster.py
@@ -82,7 +82,7 @@ class Cluster(object):
     # pylint: disable=too-many-arguments
     # walletd [True|False] Is keosd running. If not load the wallet plugin
     def __init__(self, walletd=False, localCluster=True, host="localhost", port=8888, walletHost="localhost", walletPort=9899
-                 , defproduceraPrvtKey=None, defproducerbPrvtKey=None, staging=False, loggingLevel="debug"):
+                 , defproduceraPrvtKey=None, defproducerbPrvtKey=None, staging=False, loggingLevel="debug", loggingLevelDict={}):
         """Cluster container.
         walletd [True|False] Is wallet keosd running. If not load the wallet plugin
         localCluster [True|False] Is cluster local to host.
@@ -106,6 +106,7 @@ class Cluster(object):
         self.walletPort=walletPort
         self.staging=staging
         self.loggingLevel=loggingLevel
+        self.loggingLevelDict=loggingLevelDict
         # init accounts
         self.defProducerAccounts={}
         self.defproduceraAccount=self.defProducerAccounts["defproducera"]= Account("defproducera")
@@ -241,10 +242,10 @@ class Cluster(object):
                 return False
             tries = tries - 1
             time.sleep(2)
-
-        cmd="%s -p %s -n %s -d %s -i %s -f %s --unstarted-nodes %s --logging-level %s" % (
+        loggingLevelDictString = json.dumps(self.loggingLevelDict).replace(" ", "")
+        cmd="%s -p %s -n %s -d %s -i %s -f %s --unstarted-nodes %s --logging-level %s --logging-level-map %s" % (
             Utils.EosLauncherPath, pnodes, totalNodes, delay, datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3],
-            producerFlag, unstartedNodes, self.loggingLevel)
+            producerFlag, unstartedNodes, self.loggingLevel, loggingLevelDictString)
         cmdArr=cmd.split()
         if self.staging:
             cmdArr.append("--nogen")
