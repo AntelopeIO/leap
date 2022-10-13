fd1b3c81ad92dd685b97a34b44e0f2f0efa0df6c
766C6164 <valterdaw@gmail.com>
2022-10-13 12:31:18 -0400

    Refactoring of block-log subcommand

--- programs/leap-util/actions/blocklog.hpp
+++ programs/leap-util/actions/blocklog.hpp
@@ -18,32 +18,38 @@ struct blocklog_options {
    bool no_pretty_print = false;
    bool as_json_array = false;
 
-   // subcommands
-   bool make_index = false;
-   bool trim_blocklog = false;
-   bool extract_blocks = false;
-   bool smoke_test = false;
-   bool vacuum = false;
-   bool genesis = false;
-
    std::optional<block_log_prune_config> blog_keep_prune_conf;
 };
+
 
 class blocklog_actions : public sub_command<blocklog_options> {
 public:
    blocklog_actions() : sub_command() {}
    void setup(CLI::App& app);
 
-   // callbacks
-   int run_subcommand();
+   enum class action_type : uint32_t {
+      ac_default = 0,
+      make_index = 1,
+      trim_blocklog = 2,
+      extract_blocks = 3,
+      smoke_test = 4,
+      vacuum = 5,
+      genesis = 6,
+   };
+
+   // callback
+   int run_subcommand(action_type at = action_type::ac_default);
+
+   // exception logger
+   void lippincott() noexcept;
 
 protected:
    void initialize();
    int trim_blocklog_end(bfs::path block_dir, uint32_t n);
    bool trim_blocklog_front(bfs::path block_dir, uint32_t n);
    bool extract_block_range(bfs::path block_dir, bfs::path output_dir, uint32_t start, uint32_t end);
-   void smoke_test(bfs::path block_dir);
-   int  do_vacuum();
-   int  do_genesis();
+   void smoke_test();
+   int do_vacuum();
+   int do_genesis();
    void read_log();
 };
