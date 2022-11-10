91e96d01b880906e6b659e1eac1ee16600b91c99
Jonathan Giszczak <jonrg@hypercubepc.com>
2022-11-10 15:09:14 -0600

    Move chain transactions API out of Node into its own class.
    Still avoiding modifying tests for the time being.

--- tests/TestHarness/__init__.py
+++ tests/TestHarness/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['Node', 'Cluster', 'WalletMgr', 'testUtils', 'TestHelper', 'queries']
+__all__ = ['Node', 'Cluster', 'WalletMgr', 'testUtils', 'TestHelper', 'queries', 'transactions']
 
 from .Cluster import Cluster
 from .Node import Node
