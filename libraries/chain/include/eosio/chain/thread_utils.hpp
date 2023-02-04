44434bc5f1fa94ea74c6748d575734bf14808280
Kevin Heifner <heifnerk@objectcomputing.com>
2023-02-04 14:51:12 -0600

    Use method so named_thread_pool template is on the stack. Thanks @spoonincode for the idea.

--- libraries/chain/include/eosio/chain/thread_utils.hpp
+++ libraries/chain/include/eosio/chain/thread_utils.hpp
@@ -19,6 +19,7 @@ namespace eosio { namespace chain {
    /**
     * Wrapper class for thread pool of boost asio io_context run.
     * Also names threads so that tools like htop can see thread name.
+    * Example: named_thread_pool<eosio::chain::make_name_v("chain")> thread_pool;
     * @param NamePrefix  is eosio::name appended with -## of thread.
     *                    A short name_prefix (6 chars or under) is recommended as console_appender uses 9 chars
     *                    for the thread name.
@@ -51,38 +52,7 @@ namespace eosio { namespace chain {
          _ioc.restart();
          _thread_pool.reserve( num_threads );
          for( size_t i = 0; i < num_threads; ++i ) {
-            _thread_pool.emplace_back( [&ioc = _ioc, name_prefix = _name_prefix, on_except, i]() {
-               // copying the name_prefix and i into lambda makes it available to be seen on the stack in the debugger
-               std::string tn = name_prefix.to_string() + "-" + std::to_string( i );
-               try {
-                  fc::set_os_thread_name( tn );
-                  ioc.run();
-               } catch( const fc::exception& e ) {
-                  if( on_except ) {
-                     on_except( e );
-                  } else {
-                     elog( "Exiting thread ${t} on exception: ${e}", ("t", tn)("e", e.to_detail_string()) );
-                     throw;
-                  }
-               } catch( const std::exception& e ) {
-                  fc::std_exception_wrapper se( FC_LOG_MESSAGE( warn, "${what}: ", ("what", e.what()) ),
-                                                std::current_exception(), BOOST_CORE_TYPEID( e ).name(), e.what() );
-                  if( on_except ) {
-                     on_except( se );
-                  } else {
-                     elog( "Exiting thread ${t} on exception: ${e}", ("t", tn)("e", se.to_detail_string()) );
-                     throw;
-                  }
-               } catch( ... ) {
-                  if( on_except ) {
-                     fc::unhandled_exception ue( FC_LOG_MESSAGE( warn, "unknown exception" ), std::current_exception() );
-                     on_except( ue );
-                  } else {
-                     elog( "Exiting thread ${t} on unknown exception", ("t", tn) );
-                     throw;
-                  }
-               }
-            } );
+            _thread_pool.emplace_back( std::thread( &named_thread_pool::run_thread, this, i, on_except )  );
          }
       }
 
@@ -94,6 +64,40 @@ namespace eosio { namespace chain {
             t.join();
          }
          _thread_pool.clear();
+      }
+
+   private:
+      void run_thread( size_t i, const on_except_t& on_except ) {
+         // copying the name_prefix and i into lambda makes it available to be seen on the stack in the debugger
+         std::string tn = _name_prefix.to_string() + "-" + std::to_string( i );
+         try {
+            fc::set_os_thread_name( tn );
+            _ioc.run();
+         } catch( const fc::exception& e ) {
+            if( on_except ) {
+               on_except( e );
+            } else {
+               elog( "Exiting thread ${t} on exception: ${e}", ("t", tn)("e", e.to_detail_string()) );
+               throw;
+            }
+         } catch( const std::exception& e ) {
+            fc::std_exception_wrapper se( FC_LOG_MESSAGE( warn, "${what}: ", ("what", e.what()) ),
+                                          std::current_exception(), BOOST_CORE_TYPEID( e ).name(), e.what() );
+            if( on_except ) {
+               on_except( se );
+            } else {
+               elog( "Exiting thread ${t} on exception: ${e}", ("t", tn)("e", se.to_detail_string()) );
+               throw;
+            }
+         } catch( ... ) {
+            if( on_except ) {
+               fc::unhandled_exception ue( FC_LOG_MESSAGE( warn, "unknown exception" ), std::current_exception() );
+               on_except( ue );
+            } else {
+               elog( "Exiting thread ${t} on unknown exception", ("t", tn) );
+               throw;
+            }
+         }
       }
 
    private:
