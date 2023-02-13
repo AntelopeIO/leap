ec5681d7756de1a25693d982bfdef7454c8b979f
Jonathan Giszczak <jonrg@hypercubepc.com>
2023-02-13 01:24:03 -0600

    Enum class _generate_next_value can't be staticmethod on older Pythons

--- tests/TestHarness/logging.py
+++ tests/TestHarness/logging.py
@@ -6,7 +6,6 @@ class fc_log_level(Enum): # Convert to StrEnum once Python 3.11 is minimum on su
     # Numbers are not required on the Python side
     def __str__(self):
         return self.value
-    @staticmethod
     def _generate_next_value_(name: str, start: int, count: int, last_values) -> Any:
         return name.lower()
     all = auto()
