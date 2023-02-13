9837593164a5167c409c7c3f0f628cf4acc812a3
Jonathan Giszczak <jonrg@hypercubepc.com>
2023-02-13 00:20:51 -0600

    Omit typing not understood on most platforms.

--- tests/TestHarness/logging.py
+++ tests/TestHarness/logging.py
@@ -7,7 +7,7 @@ class fc_log_level(Enum): # Convert to StrEnum once Python 3.11 is minimum on su
     def __str__(self):
         return self.value
     @staticmethod
-    def _generate_next_value_(name: str, start: int, count: int, last_values: list[Any]) -> Any:
+    def _generate_next_value_(name: str, start: int, count: int, last_values) -> Any:
         return name.lower()
     all = auto()
     debug = auto()
