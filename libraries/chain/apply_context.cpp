2ae98cb29c83dd8603e6fd8ec6e3ed985ab11e08
Lin Huang <lin.huang@eosnetwork.com>
2023-03-02 07:53:42 -0500

    correct bad comments about dm logging and improve error message for scheduling/cacelling deferred transactions

--- libraries/chain/apply_context.cpp
+++ libraries/chain/apply_context.cpp
@@ -426,7 +426,7 @@ void apply_context::execute_context_free_inline( action&& a ) {
 
 
 void apply_context::schedule_deferred_transaction( const uint128_t& sender_id, account_name payer, transaction&& trx, bool replace_existing ) {
-   EOS_ASSERT( !trx_context.is_read_only(), transaction_exception, "cannot schedule a readonly deferred transaction" );
+   EOS_ASSERT( !trx_context.is_read_only(), transaction_exception, "cannot schedule a deferred transaction from within a readonly transaction" );
    EOS_ASSERT( trx.context_free_actions.size() == 0, cfa_inside_generated_tx, "context free actions are not currently allowed in generated transactions" );
 
    bool enforce_actor_whitelist_blacklist = trx_context.enforce_whiteblacklist && control.is_speculative_block()
@@ -627,7 +627,7 @@ void apply_context::schedule_deferred_transaction( const uint128_t& sender_id, a
 }
 
 bool apply_context::cancel_deferred_transaction( const uint128_t& sender_id, account_name sender ) {
-   EOS_ASSERT( !trx_context.is_read_only(), transaction_exception, "cannot cancel a readonly deferred transaction" );
+   EOS_ASSERT( !trx_context.is_read_only(), transaction_exception, "cannot cancel a deferred transaction from within a readonly transaction" );
    auto& generated_transaction_idx = db.get_mutable_index<generated_transaction_multi_index>();
    const auto* gto = db.find<generated_transaction_object,by_sender_id>(boost::make_tuple(sender, sender_id));
    if ( gto ) {
