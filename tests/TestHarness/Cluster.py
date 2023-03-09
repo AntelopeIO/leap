c2c784fb25944831c0f6c020b1a5de6ef0af4043
Kevin Heifner <heifnerk@objectcomputing.com>
2023-03-09 14:09:28 -0600

    GH-789 Fix regex

--- tests/TestHarness/Cluster.py
+++ tests/TestHarness/Cluster.py
@@ -969,7 +969,7 @@ class Cluster(object):
         with open(configFile, 'r') as f:
             configStr=f.read()
 
-        pattern=r"^\s*signature-provider\s*=(\w+)=KEY:(\w+)$"
+        pattern=r"^\s*signature-provider\s*=\s*(\w+)=KEY:(\w+)$"
         m=re.search(pattern, configStr, re.MULTILINE)
         regMsg="None" if m is None else "NOT None"
         if m is None:
