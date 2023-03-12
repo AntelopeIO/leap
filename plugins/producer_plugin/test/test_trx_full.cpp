309609e62be2f3c862ba3c137286a75870d343da
Kevin Heifner <heifnerk@objectcomputing.com>
2023-03-11 23:01:11 -0600

    max-transaction-time is required to be less than read-only-read-window-time-us

--- plugins/producer_plugin/test/test_trx_full.cpp
+++ plugins/producer_plugin/test/test_trx_full.cpp
@@ -112,7 +112,7 @@ BOOST_AUTO_TEST_CASE(producer) {
          fc::logger::get(DEFAULT_LOGGER).set_log_level(fc::log_level::debug);
          std::vector<const char*> argv =
                {"test", "--data-dir", temp_dir_str.c_str(), "--config-dir", temp_dir_str.c_str(),
-                "-p", "eosio", "-e", "--max-transaction-time", "475", "--disable-subjective-billing=true" };
+                "-p", "eosio", "-e", "--disable-subjective-billing=true" };
          app->initialize<chain_plugin, producer_plugin>( argv.size(), (char**) &argv[0] );
          app->startup();
          plugin_promise.set_value(
