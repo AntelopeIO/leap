726542d54963a99c31556604c5f11c9cf862b3b2
Zach Butler <zach.butler@eosnetwork.com>
2023-03-16 23:15:01 -0400

    Whitespace

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 apt-get update
 apt-get update --fix-missing
-DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt-get -y install tzdata
+DEBIAN_FRONTEND=noninteractive
+TZ=Etc/UTC
+apt-get -y install tzdata
 apt-get -y install zip unzip libncurses5 wget git build-essential cmake curl libgmp-dev libssl-dev libzstd-dev time zlib1g-dev libtinfo-dev bzip2 libbz2-dev python3 python3-numpy file
