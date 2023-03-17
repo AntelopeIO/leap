939856411867cadef5868f6885666e9fc5adc35e
Zach Butler <zach.butler@eosnetwork.com>
2023-03-16 23:16:12 -0400

    Quote strings

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get update --fix-missing
-DEBIAN_FRONTEND=noninteractive
-TZ=Etc/UTC
+DEBIAN_FRONTEND='noninteractive'
+TZ='Etc/UTC'
 apt-get install -y tzdata
 apt-get install -y zip unzip libncurses5 wget git build-essential cmake curl libgmp-dev libssl-dev libzstd-dev time zlib1g-dev libtinfo-dev bzip2 libbz2-dev python3 python3-numpy file
