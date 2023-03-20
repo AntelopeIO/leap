958f6d53b6dcd71c1eae08b019423cf8024eab57
Zach Butler <zach.butler@eosnetwork.com>
2023-03-20 18:00:26 -0400

    tzdata does not need to be separate

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,7 +3,6 @@ apt-get update
 apt-get update --fix-missing
 DEBIAN_FRONTEND='noninteractive'
 TZ='Etc/UTC'
-apt-get install -y tzdata
 apt-get install -y \
     build-essential \
     bzip2 \
@@ -21,6 +20,7 @@ apt-get install -y \
     python3 \
     python3-numpy \
     time \
+    tzdata \
     unzip \
     wget \
     zip \
