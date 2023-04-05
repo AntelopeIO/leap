3f9f7045879dcac367e482b596330f1c2c799616
greg7mdp <greg7mdp@gmail.com>
2023-04-05 13:14:46 -0400

    Missed another needed change

--- plugins/http_plugin/include/eosio/http_plugin/macros.hpp
+++ plugins/http_plugin/include/eosio/http_plugin/macros.hpp
@@ -8,26 +8,28 @@ struct async_result_visitor : public fc::visitor<fc::variant> {
 };
 
 #define CALL_ASYNC_WITH_400(api_name, api_handle, api_namespace, call_name, call_result, http_response_code, params_type) \
-{std::string("/v1/" #api_name "/" #call_name), \
-   [api_handle](string&&, string&& body, url_response_callback&& cb) mutable { \
-      auto deadline = api_handle.start(); \
-      try { \
-         auto params = parse_params<api_namespace::call_name ## _params, params_type>(body);\
-         FC_CHECK_DEADLINE(deadline);\
-         api_handle.call_name( std::move(params), \
+{std::string("/v1/" #api_name "/" #call_name),                                                                   \
+   [api_handle](string&&, string&& body, url_response_callback&& cb) mutable {                                   \
+      auto deadline = api_handle.start();                                                                        \
+      try {                                                                                                      \
+         auto params = parse_params<api_namespace::call_name ## _params, params_type>(body);                     \
+         FC_CHECK_DEADLINE(deadline);                                                                            \
+         api_handle.call_name( std::move(params),                                                                \
                [cb=std::move(cb), body=std::move(body)](const eosio::chain::next_function_variant<call_result>& result){ \
-               if (std::holds_alternative<fc::exception_ptr>(result)) {\
-                  try {\
-                     std::get<fc::exception_ptr>(result)->dynamic_rethrow_exception();\
-                  } catch (...) {\
-                     http_plugin::handle_exception(#api_name, #call_name, body, cb);\
-                  }\
-               } else {\
-                  cb(http_response_code, fc::time_point::maximum(), std::visit(async_result_visitor(), result));\
-               }\
-            });\
-      } catch (...) { \
-         http_plugin::handle_exception(#api_name, #call_name, body, cb); \
-      } \
-   }\
+               if (std::holds_alternative<fc::exception_ptr>(result)) {                                          \
+                  try {                                                                                          \
+                     std::get<fc::exception_ptr>(result)->dynamic_rethrow_exception();                           \
+                  } catch (...) {                                                                                \
+                     http_plugin::handle_exception(#api_name, #call_name, body, cb);                             \
+                  }                                                                                              \
+               } else if (std::holds_alternative<call_result>(result)) {                                      \
+                  cb(http_response_code, fc::time_point::maximum(), fc::variant(std::get<call_result>(result))); \
+               } else {                                                                                          \
+                  assert(0);                                                                                     \
+               }                                                                                                 \
+            });                                                                                                  \
+      } catch (...) {                                                                                            \
+         http_plugin::handle_exception(#api_name, #call_name, body, cb);                                         \
+      }                                                                                                          \
+   }                                                                                                             \
 }
