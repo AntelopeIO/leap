e58d26fb833fb0e7c319dc3f19b8708f2c2fa110
Lin Huang <lin.huang@eosnetwork.com>
2023-04-07 12:36:44 -0400

    Replace ro_trx_temporary ro_trx_vm_oc_compile_temporary_failure, ro_trx_permanent with ro_trx_vm_oc_compile_permanent_failure, and add better description

--- libraries/chain/include/eosio/chain/exceptions.hpp
+++ libraries/chain/include/eosio/chain/exceptions.hpp
@@ -276,10 +276,6 @@ namespace eosio { namespace chain {
                                     3040017, "Transaction includes disallowed extensions (invalid block)" )
       FC_DECLARE_DERIVED_EXCEPTION( tx_resource_exhaustion, transaction_exception,
                                     3040018, "Transaction exceeded transient resource limit" )
-      FC_DECLARE_DERIVED_EXCEPTION( ro_trx_temporary, transaction_exception,
-                                    3040019, "Read-only transaction temporary failure" )
-      FC_DECLARE_DERIVED_EXCEPTION( ro_trx_permanent, transaction_exception,
-                                    3040020, "Read-only transaction permanent failure" )
 
 
    FC_DECLARE_DERIVED_EXCEPTION( action_validate_exception, chain_exception,
@@ -365,6 +361,10 @@ namespace eosio { namespace chain {
                                     3080007, "Transaction exceeded the current greylisted account network usage limit" )
       FC_DECLARE_DERIVED_EXCEPTION( greylist_cpu_usage_exceeded, resource_exhausted_exception,
                                     3080008, "Transaction exceeded the current greylisted account CPU usage limit" )
+      FC_DECLARE_DERIVED_EXCEPTION( ro_trx_vm_oc_compile_temporary_failure, resource_exhausted_exception,
+                                    3080009, "Read-only transaction eos-vm-oc compile temporary failure" )
+      FC_DECLARE_DERIVED_EXCEPTION( ro_trx_vm_oc_compile_permanent_failure, resource_exhausted_exception,
+                                    3080010, "Read-only transaction eos-vm-oc compile permanent failure" )
 
       FC_DECLARE_DERIVED_EXCEPTION( leeway_deadline_exception, deadline_exception,
                                     3081001, "Transaction reached the deadline set due to leeway on account CPU limits" )
