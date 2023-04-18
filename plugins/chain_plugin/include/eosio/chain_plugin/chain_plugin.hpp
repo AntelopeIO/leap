41e2e68e3fa7ea4a1780e7690ad1d62e70b2feff
greg7mdp <greg7mdp@gmail.com>
2023-04-18 14:10:28 -0400

    Update `get_table_rows_ex()` to serialize off the main thread.

--- plugins/chain_plugin/include/eosio/chain_plugin/chain_plugin.hpp
+++ plugins/chain_plugin/include/eosio/chain_plugin/chain_plugin.hpp
@@ -638,13 +638,14 @@ public:
          auto walk_table_row_range = [&]( auto itr, auto end_itr ) {
             auto cur_time = fc::time_point::now();
             vector<char> data;
-            for( unsigned int count = 0; cur_time <= params_deadline && count < p.limit && itr != end_itr; ++itr, cur_time = fc::time_point::now() ) {
+            for( unsigned int count = 0;
+                 cur_time <= params_deadline && count < p.limit && itr != end_itr;
+                 ++count, ++itr, cur_time = fc::time_point::now() ) {
                FC_CHECK_DEADLINE(deadline);
                const auto* itr2 = d.find<chain::key_value_object, chain::by_scope_primary>( boost::make_tuple(t_id->id, itr->primary_key) );
                if( itr2 == nullptr ) continue;
                copy_inline_row(*itr2, data);
                http_params.rows.emplace_back(std::move(data), itr->payer);
-               ++count;
             }
             if( itr != end_itr ) {
                http_params.more = true;
@@ -660,32 +661,35 @@ public:
             walk_table_row_range( lower, upper );
          }
       }
-      
+
+      // not enforcing the deadline for that second processing part (the serialization), as it is not taking place
+      // on the main thread, but in the http thread pool.
       return [p = std::move(http_params), abi=std::move(abi), abi_serializer_max_time=abi_serializer_max_time]() mutable ->
          chain::t_or_exception<read_only::get_table_rows_result> {
          read_only::get_table_rows_result result;
          abi_serializer abis;
          abis.set_abi(std::move(abi), abi_serializer::create_yield_function(abi_serializer_max_time));
          auto table_type = abis.get_table_type(p.table);
+         
          for (auto& row : p.rows) {
-               fc::variant data_var;
-               if( p.json ) {
-                  data_var = abis.binary_to_variant(table_type, row.first,
-                                                    abi_serializer::create_yield_function(abi_serializer_max_time),
-                                                    p.shorten_abi_errors );
-               } else {
-                  data_var = fc::variant(row.first);
-               }
+            fc::variant data_var;
+            if( p.json ) {
+               data_var = abis.binary_to_variant(table_type, row.first,
+                                                 abi_serializer::create_yield_function(abi_serializer_max_time),
+                                                 p.shorten_abi_errors );
+            } else {
+               data_var = fc::variant(row.first);
+            }
 
-               if (p.show_payer) {
-                  result.rows.emplace_back(fc::mutable_variant_object("data", std::move(data_var))("payer", row.second));
-               } else {
-                  result.rows.emplace_back(std::move(data_var));
-               }            
+            if (p.show_payer) {
+               result.rows.emplace_back(fc::mutable_variant_object("data", std::move(data_var))("payer", row.second));
+            } else {
+               result.rows.emplace_back(std::move(data_var));
+            }            
          }
          result.more = p.more;
          result.next_key = p.next_key;
-         return std::move(result);
+         return result;
       };
    }
 
@@ -693,18 +697,27 @@ public:
    get_table_rows_return_t
    get_table_rows_ex( const read_only::get_table_rows_params& p,
                       abi_def&& abi,
-                      const fc::time_point& deadline )const {
+                      const fc::time_point& deadline ) const {
 
       fc::microseconds params_time_limit = p.time_limit_ms ? fc::milliseconds(*p.time_limit_ms) : fc::milliseconds(10);
       fc::time_point params_deadline = fc::time_point::now() + params_time_limit;
 
-      read_only::get_table_rows_result result;
+      struct http_params_t {
+         name table;
+         bool shorten_abi_errors;
+         bool json;
+         bool show_payer;
+         bool more;
+         std::string next_key;
+         vector<std::pair<vector<char>, name>> rows;
+      };
+      
+      http_params_t http_params { p.table, shorten_abi_errors, p.json, p.show_payer && *p.show_payer, false  };
+         
       const auto& d = db.db();
 
       uint64_t scope = convert_to_type<uint64_t>(p.scope, "scope");
 
-      abi_serializer abis;
-      abis.set_abi(std::move(abi), abi_serializer::create_yield_function( abi_serializer_max_time ));
       const auto* t_id = d.find<chain::table_id_object, chain::by_code_scope_table>(boost::make_tuple(p.code, name(scope), p.table));
       if( t_id != nullptr ) {
          const auto& idx = d.get_index<IndexType, chain::by_scope_primary>();
@@ -739,26 +752,16 @@ public:
          auto walk_table_row_range = [&]( auto itr, auto end_itr ) {
             auto cur_time = fc::time_point::now();
             vector<char> data;
-            for( unsigned int count = 0; cur_time <= params_deadline && count < p.limit && itr != end_itr; ++count, ++itr, cur_time = fc::time_point::now() ) {
+            for( unsigned int count = 0;
+                 cur_time <= params_deadline && count < p.limit && itr != end_itr;
+                 ++count, ++itr, cur_time = fc::time_point::now() ) {
                FC_CHECK_DEADLINE(deadline);
                copy_inline_row(*itr, data);
-
-               fc::variant data_var;
-               if( p.json ) {
-                  data_var = abis.binary_to_variant( abis.get_table_type(p.table), data, abi_serializer::create_yield_function( abi_serializer_max_time ), shorten_abi_errors );
-               } else {
-                  data_var = fc::variant( data );
-               }
-
-               if( p.show_payer && *p.show_payer ) {
-                  result.rows.emplace_back( fc::mutable_variant_object("data", std::move(data_var))("payer", itr->payer) );
-               } else {
-                  result.rows.emplace_back( std::move(data_var) );
-               }
+               http_params.rows.emplace_back(std::move(data), itr->payer);
             }
             if( itr != end_itr ) {
-               result.more = true;
-               result.next_key = convert_to_string(itr->primary_key, p.key_type, p.encode_type, "next_key - next lower bound");
+               http_params.more = true;
+               http_params.next_key = convert_to_string(itr->primary_key, p.key_type, p.encode_type, "next_key - next lower bound");
             }
          };
 
@@ -770,8 +773,35 @@ public:
             walk_table_row_range( lower, upper );
          }
       }
-      return [result = std::move(result)]() mutable -> chain::t_or_exception<read_only::get_table_rows_result> {
-         return std::move(result);
+      
+      // not enforcing the deadline for that second processing part (the serialization), as it is not taking place
+      // on the main thread, but in the http thread pool.
+      return [p = std::move(http_params), abi=std::move(abi), abi_serializer_max_time=abi_serializer_max_time]() mutable ->
+         chain::t_or_exception<read_only::get_table_rows_result> {
+         read_only::get_table_rows_result result;
+         abi_serializer abis;
+         abis.set_abi(std::move(abi), abi_serializer::create_yield_function(abi_serializer_max_time));
+         auto table_type = abis.get_table_type(p.table);
+         
+         for (auto& row : p.rows) {
+            fc::variant data_var;
+            if( p.json ) {
+               data_var = abis.binary_to_variant(table_type, row.first,
+                                                 abi_serializer::create_yield_function(abi_serializer_max_time),
+                                                 p.shorten_abi_errors );
+            } else {
+               data_var = fc::variant(row.first);
+            }
+
+            if (p.show_payer) {
+               result.rows.emplace_back(fc::mutable_variant_object("data", std::move(data_var))("payer", row.second));
+            } else {
+               result.rows.emplace_back(std::move(data_var));
+            }            
+         }
+         result.more = p.more;
+         result.next_key = p.next_key;
+         return result;
       };
    }
 
