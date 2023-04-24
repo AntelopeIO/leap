f5cb026ee1e18c0d5997552cf147d53bb60196b2
greg7mdp <greg7mdp@gmail.com>
2023-04-24 14:25:08 -0400

    Lower another timeout which I hit occasionally when running tests.

--- unittests/api_tests.cpp
+++ unittests/api_tests.cpp
@@ -1349,7 +1349,7 @@ BOOST_FIXTURE_TEST_CASE(checktime_hashing_fail, TESTER) { try {
 
         //hit deadline exception, but cache the contract
         BOOST_CHECK_EXCEPTION( call_test( *this, test_api_action<TEST_METHOD("test_checktime", "checktime_sha1_failure")>{},
-                                          5000, 10, 10 ),
+                                          5000, 8, 8 ),
                                deadline_exception, is_deadline_exception );
 
         BOOST_TEST( is_code_cached("testapi"_n) );
