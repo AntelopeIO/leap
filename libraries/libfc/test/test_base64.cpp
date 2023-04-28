ca7e9b1ae4b788aea17cf2b4e44eb4ca9b2924dc
Kevin Heifner <heifnerk@objectcomputing.com>
2023-04-28 07:12:00 -0500

    GH-641 Add an explicit main

--- libraries/libfc/test/test_base64.cpp
+++ libraries/libfc/test/test_base64.cpp
@@ -1,5 +1,4 @@
-#define BOOST_TEST_MODULE libfc
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include <fc/crypto/base64.hpp>
 #include <fc/exception/exception.hpp>
