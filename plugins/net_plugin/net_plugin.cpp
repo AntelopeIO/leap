73644fd0afebca860afbcf7ae1d44b16e8131b5f
Kevin Heifner <heifnerk@objectcomputing.com>
2023-06-07 09:54:11 -0500

    GH-1072 Revert: No reason to post if syncing from peer.

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -3714,12 +3714,10 @@ namespace eosio {
    void net_plugin_impl::on_accepted_block_header(const block_state_ptr& bs) {
       update_chain_info();
 
-      if (!my_impl->sync_master->syncing_from_peer()) {
-         dispatcher->strand.post([bs]() {
-            fc_dlog(logger, "signaled accepted_block_header, blk num = ${num}, id = ${id}", ("num", bs->block_num)("id", bs->id));
-            my_impl->dispatcher->bcast_block(bs->block, bs->id);
-         });
-      }
+      dispatcher->strand.post([bs]() {
+         fc_dlog(logger, "signaled accepted_block_header, blk num = ${num}, id = ${id}", ("num", bs->block_num)("id", bs->id));
+         my_impl->dispatcher->bcast_block(bs->block, bs->id);
+      });
    }
 
    void net_plugin_impl::on_accepted_block(const block_state_ptr& ) {
