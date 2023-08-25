7388d8e4c5f158a10fe1a89ec004afd142689260
Areg Hayrapetian <arhag@users.noreply.github.com>
2023-08-25 16:27:23 -0700

    Remove deleted functions so that Leap 4.0 can be used to compiled evm-bridge-contract tests.

--- libraries/chain/include/eosio/chain/unapplied_transaction_queue.hpp
+++ libraries/chain/include/eosio/chain/unapplied_transaction_queue.hpp
@@ -37,11 +37,6 @@ struct unapplied_transaction {
 
    const transaction_id_type& id()const { return trx_meta->id(); }
    fc::time_point_sec expiration()const { return trx_meta->packed_trx()->expiration(); }
-
-   unapplied_transaction(const unapplied_transaction&) = delete;
-   unapplied_transaction() = delete;
-   unapplied_transaction& operator=(const unapplied_transaction&) = delete;
-   unapplied_transaction(unapplied_transaction&&) = default;
 };
 
 /**
