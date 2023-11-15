0c6c2b5a96e369cb5323314bdedff7ff456bf73c
Kevin Heifner <heifnerk@objectcomputing.com>
2023-11-15 09:13:49 -0600

    net_plugin test cleanup

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -1,5 +1,6 @@
 #include <eosio/net_plugin/net_plugin.hpp>
 #include <eosio/net_plugin/protocol.hpp>
+#include <eosio/net_plugin/net_utils.hpp>
 #include <eosio/net_plugin/auto_bp_peering.hpp>
 #include <eosio/chain/types.hpp>
 #include <eosio/chain/controller.hpp>
@@ -517,10 +518,6 @@ namespace eosio {
       std::function<void()> increment_dropped_trxs;
       
    private:
-      inline static const std::map<std::string, size_t> prefix_multipliers{
-         {"",1},{"K",pow(10,3)},{"M",pow(10,6)},{"G",pow(10, 9)},{"T",pow(10, 12)},
-                {"Ki",pow(2,10)},{"Mi",pow(2,20)},{"Gi",pow(2,30)},{"Ti",pow(2,40)}
-      };
       alignas(hardware_destructive_interference_size)
       mutable fc::mutex             chain_info_mtx; // protects chain_info_t
       chain_info_t                  chain_info GUARDED_BY(chain_info_mtx);
@@ -575,8 +572,6 @@ namespace eosio {
 
       constexpr static uint16_t to_protocol_version(uint16_t v);
 
-      std::tuple<std::string, size_t> parse_listen_address(const std::string& peer) const;
-      size_t parse_connection_rate_limit(const string& limit_str) const;
       void plugin_initialize(const variables_map& options);
       void plugin_startup();
       void plugin_shutdown();
@@ -4104,52 +4099,6 @@ namespace eosio {
       return fc::json::from_string(s).as<T>();
    }
 
-   std::tuple<std::string, size_t> net_plugin_impl::parse_listen_address( const std::string& address ) const {
-      auto listen_addr = address;
-      auto limit = string("0");
-      auto last_colon_location = address.rfind(':');
-      if( auto right_bracket_location = address.find(']'); right_bracket_location != address.npos ) {
-         if( std::count(address.begin()+right_bracket_location, address.end(), ':') > 1 ) {
-            listen_addr = std::string(address, 0, last_colon_location);
-            limit = std::string(address, last_colon_location+1);
-         }
-      } else {
-         if( auto colon_count = std::count(address.begin(), address.end(), ':'); colon_count > 1 ) {
-            EOS_ASSERT( colon_count <= 2, plugin_config_exception, "Invalid address specification ${addr}; IPv6 addresses must be enclosed in square brackets.", ("addr", address));
-            listen_addr = std::string(address, 0, last_colon_location);
-            limit = std::string(address, last_colon_location+1);
-         }
-      }
-      auto block_sync_rate_limit = parse_connection_rate_limit(limit);
-
-      return {listen_addr, block_sync_rate_limit};
-   }
-
-   size_t net_plugin_impl::parse_connection_rate_limit( const std::string& limit_str) const {
-      std::istringstream in(limit_str);
-      double limit{0};
-      in >> limit;
-      EOS_ASSERT(limit >= 0.0, plugin_config_exception, "block sync rate limit must not be negative: ${limit}", ("limit", limit_str));
-      size_t block_sync_rate_limit = 0;
-      if( limit > 0.0 ) {
-         std::string units;
-         in >> units;
-         std::regex units_regex{"([KMGT]?[i]?)B/s"};
-         std::smatch units_match;
-         std::regex_match(units, units_match, units_regex);
-         if( units.length() > 0 ) {
-            EOS_ASSERT(units_match.size() == 2, plugin_config_exception, "invalid block sync rate limit specification: ${limit}", ("limit", units));
-            try {
-               block_sync_rate_limit = boost::numeric_cast<size_t>(limit * prefix_multipliers.at(units_match[1].str()));
-               fc_ilog( logger, "setting block_sync_rate_limit to ${limit} megabytes per second", ("limit", double(block_sync_rate_limit)/1000000));
-            } catch (boost::numeric::bad_numeric_cast&) {
-               EOS_THROW(plugin_config_exception, "block sync rate limit specification overflowed: ${limit}", ("limit", limit_str));
-            }
-         }
-      }
-      return block_sync_rate_limit;
-   }
-
    void net_plugin_impl::plugin_initialize( const variables_map& options ) {
       try {
          fc_ilog( logger, "Initialize net plugin" );
@@ -4365,7 +4314,8 @@ namespace eosio {
                std::string extra_listening_log_info =
                      ", max clients is " + std::to_string(my->connections.get_max_client_count());
 
-               auto [listen_addr, block_sync_rate_limit] = my->parse_listen_address(address);
+               auto [listen_addr, block_sync_rate_limit] = net_utils::parse_listen_address(address);
+               fc_ilog( logger, "setting block_sync_rate_limit to ${limit} megabytes per second", ("limit", double(block_sync_rate_limit)/1000000));
 
                fc::create_listener<tcp>(
                      my->thread_pool.get_executor(), logger, accept_timeout, listen_addr, extra_listening_log_info,
---
+++ plugins/net_plugin/tests/main.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE net_plugin
+#include <boost/test/included/unit_test.hpp>
