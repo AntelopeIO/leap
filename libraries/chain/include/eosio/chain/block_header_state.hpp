87c7cbd59928b6c3930e1953ef811a540545fb66
Lin Huang <lin.huang@eosnetwork.com>
2023-11-22 10:05:38 -0500

    make comments in longer lines

--- libraries/chain/include/eosio/chain/block_header_state.hpp
+++ libraries/chain/include/eosio/chain/block_header_state.hpp
@@ -126,16 +126,15 @@ protected:
  *  A data structure holding hotstuff core information
  */
 struct block_header_state_core {
-   uint32_t last_final_block_height = 0; // the block height of the last irreversible
-                                         // (final) block.
-   std::optional<uint32_t> final_on_strong_qc_block_height; // the block height of
-                                                            // the block that would
-                                                            // become irreversible
-                                                            // if the associated block
-                                                            // header was to achieve a
-                                                            // strong QC.
-   std::optional<uint32_t> last_qc_block_height; // the block height of the block that
-                                                 // is referenced as the last QC block
+   // the block height of the last irreversible (final) block.
+   uint32_t last_final_block_height = 0;
+
+   // the block height of the block that would become irreversible (final) if the
+   // associated block header was to achieve a strong QC.
+   std::optional<uint32_t> final_on_strong_qc_block_height;
+
+   // the block height of the block that is referenced as the last QC block
+   std::optional<uint32_t> last_qc_block_height;
 
    block_header_state_core() = default;
 
