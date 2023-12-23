d50879597dc8ee318795493ee45c1e22134034a6
greg7mdp <greg7mdp@gmail.com>
2023-12-22 22:24:29 -0500

    Remove outdated comment and fix indentation.

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -2540,7 +2540,6 @@ struct controller_impl {
          auto add_completed_block = [&](auto& fork_db, auto& head) {
             const auto& bsp = std::get<std::decay_t<decltype(head)>>(cb.bsp);
 
-            // [greg todo] fork_db version with block_state_ptr
             if( s == controller::block_status::incomplete ) {
                fork_db.add( bsp );
                fork_db.mark_valid( bsp );
@@ -2673,113 +2672,113 @@ struct controller_impl {
       try {
          // [greg todo] remove `if`, `lambda` and `apply_dpos`, and make code work for both versions of BSP
          if constexpr (std::is_same_v<block_state_legacy_ptr,BSP>) {
-         auto do_the_work = [&](auto& fork_db, auto& head) {
-            auto start = fc::time_point::now();
-            const signed_block_ptr& b = bsp->block;
-            const auto& new_protocol_feature_activations = bsp->get_new_protocol_feature_activations();
+            auto do_the_work = [&](auto& fork_db, auto& head) {
+               auto start = fc::time_point::now();
+               const signed_block_ptr& b = bsp->block;
+               const auto& new_protocol_feature_activations = bsp->get_new_protocol_feature_activations();
 
-            auto producer_block_id = bsp->id();
-            start_block( b->timestamp, b->confirmed, new_protocol_feature_activations, s, producer_block_id, fc::time_point::maximum() );
+               auto producer_block_id = bsp->id();
+               start_block( b->timestamp, b->confirmed, new_protocol_feature_activations, s, producer_block_id, fc::time_point::maximum() );
 
-            // validated in create_block_state_future()
-            std::get<building_block>(pending->_block_stage).trx_mroot_or_receipt_digests() = b->transaction_mroot;
+               // validated in create_block_state_future()
+               std::get<building_block>(pending->_block_stage).trx_mroot_or_receipt_digests() = b->transaction_mroot;
 
-            const bool existing_trxs_metas = !bsp->trxs_metas().empty();
-            const bool pub_keys_recovered = bsp->is_pub_keys_recovered();
-            const bool skip_auth_checks = self.skip_auth_check();
-            std::vector<std::tuple<transaction_metadata_ptr, recover_keys_future>> trx_metas;
-            bool use_bsp_cached = false;
-            if( pub_keys_recovered || (skip_auth_checks && existing_trxs_metas) ) {
-               use_bsp_cached = true;
-            } else {
-               trx_metas.reserve( b->transactions.size() );
-               for( const auto& receipt : b->transactions ) {
-                  if( std::holds_alternative<packed_transaction>(receipt.trx)) {
-                     const auto& pt = std::get<packed_transaction>(receipt.trx);
-                     transaction_metadata_ptr trx_meta_ptr = trx_lookup ? trx_lookup( pt.id() ) : transaction_metadata_ptr{};
-                     if( trx_meta_ptr && *trx_meta_ptr->packed_trx() != pt ) trx_meta_ptr = nullptr;
-                     if( trx_meta_ptr && ( skip_auth_checks || !trx_meta_ptr->recovered_keys().empty() ) ) {
-                        trx_metas.emplace_back( std::move( trx_meta_ptr ), recover_keys_future{} );
-                     } else if( skip_auth_checks ) {
-                        packed_transaction_ptr ptrx( b, &pt ); // alias signed_block_ptr
-                        trx_metas.emplace_back(
-                           transaction_metadata::create_no_recover_keys( std::move(ptrx), transaction_metadata::trx_type::input ),
-                           recover_keys_future{} );
-                     } else {
-                        packed_transaction_ptr ptrx( b, &pt ); // alias signed_block_ptr
-                        auto fut = transaction_metadata::start_recover_keys(
-                           std::move( ptrx ), thread_pool.get_executor(), chain_id, fc::microseconds::maximum(), transaction_metadata::trx_type::input  );
-                        trx_metas.emplace_back( transaction_metadata_ptr{}, std::move( fut ) );
+               const bool existing_trxs_metas = !bsp->trxs_metas().empty();
+               const bool pub_keys_recovered = bsp->is_pub_keys_recovered();
+               const bool skip_auth_checks = self.skip_auth_check();
+               std::vector<std::tuple<transaction_metadata_ptr, recover_keys_future>> trx_metas;
+               bool use_bsp_cached = false;
+               if( pub_keys_recovered || (skip_auth_checks && existing_trxs_metas) ) {
+                  use_bsp_cached = true;
+               } else {
+                  trx_metas.reserve( b->transactions.size() );
+                  for( const auto& receipt : b->transactions ) {
+                     if( std::holds_alternative<packed_transaction>(receipt.trx)) {
+                        const auto& pt = std::get<packed_transaction>(receipt.trx);
+                        transaction_metadata_ptr trx_meta_ptr = trx_lookup ? trx_lookup( pt.id() ) : transaction_metadata_ptr{};
+                        if( trx_meta_ptr && *trx_meta_ptr->packed_trx() != pt ) trx_meta_ptr = nullptr;
+                        if( trx_meta_ptr && ( skip_auth_checks || !trx_meta_ptr->recovered_keys().empty() ) ) {
+                           trx_metas.emplace_back( std::move( trx_meta_ptr ), recover_keys_future{} );
+                        } else if( skip_auth_checks ) {
+                           packed_transaction_ptr ptrx( b, &pt ); // alias signed_block_ptr
+                           trx_metas.emplace_back(
+                              transaction_metadata::create_no_recover_keys( std::move(ptrx), transaction_metadata::trx_type::input ),
+                              recover_keys_future{} );
+                        } else {
+                           packed_transaction_ptr ptrx( b, &pt ); // alias signed_block_ptr
+                           auto fut = transaction_metadata::start_recover_keys(
+                              std::move( ptrx ), thread_pool.get_executor(), chain_id, fc::microseconds::maximum(), transaction_metadata::trx_type::input  );
+                           trx_metas.emplace_back( transaction_metadata_ptr{}, std::move( fut ) );
+                        }
                      }
                   }
                }
-            }
 
-            transaction_trace_ptr trace;
+               transaction_trace_ptr trace;
 
-            size_t packed_idx = 0;
-            const auto& trx_receipts = std::get<building_block>(pending->_block_stage).pending_trx_receipts();
-            for( const auto& receipt : b->transactions ) {
-               auto num_pending_receipts = trx_receipts.size();
-               if( std::holds_alternative<packed_transaction>(receipt.trx) ) {
-                  const auto& trx_meta = ( use_bsp_cached ? bsp->trxs_metas().at( packed_idx )
-                                           : ( !!std::get<0>( trx_metas.at( packed_idx ) ) ?
-                                               std::get<0>( trx_metas.at( packed_idx ) )
-                                               : std::get<1>( trx_metas.at( packed_idx ) ).get() ) );
-                  trace = push_transaction( trx_meta, fc::time_point::maximum(), fc::microseconds::maximum(), receipt.cpu_usage_us, true, 0 );
-                  ++packed_idx;
-               } else if( std::holds_alternative<transaction_id_type>(receipt.trx) ) {
-                  trace = push_scheduled_transaction( std::get<transaction_id_type>(receipt.trx), fc::time_point::maximum(), fc::microseconds::maximum(), receipt.cpu_usage_us, true );
-               } else {
-                  EOS_ASSERT( false, block_validate_exception, "encountered unexpected receipt type" );
+               size_t packed_idx = 0;
+               const auto& trx_receipts = std::get<building_block>(pending->_block_stage).pending_trx_receipts();
+               for( const auto& receipt : b->transactions ) {
+                  auto num_pending_receipts = trx_receipts.size();
+                  if( std::holds_alternative<packed_transaction>(receipt.trx) ) {
+                     const auto& trx_meta = ( use_bsp_cached ? bsp->trxs_metas().at( packed_idx )
+                                              : ( !!std::get<0>( trx_metas.at( packed_idx ) ) ?
+                                                  std::get<0>( trx_metas.at( packed_idx ) )
+                                                  : std::get<1>( trx_metas.at( packed_idx ) ).get() ) );
+                     trace = push_transaction( trx_meta, fc::time_point::maximum(), fc::microseconds::maximum(), receipt.cpu_usage_us, true, 0 );
+                     ++packed_idx;
+                  } else if( std::holds_alternative<transaction_id_type>(receipt.trx) ) {
+                     trace = push_scheduled_transaction( std::get<transaction_id_type>(receipt.trx), fc::time_point::maximum(), fc::microseconds::maximum(), receipt.cpu_usage_us, true );
+                  } else {
+                     EOS_ASSERT( false, block_validate_exception, "encountered unexpected receipt type" );
+                  }
+
+                  bool transaction_failed =  trace && trace->except;
+                  bool transaction_can_fail = receipt.status == transaction_receipt_header::hard_fail && std::holds_alternative<transaction_id_type>(receipt.trx);
+                  if( transaction_failed && !transaction_can_fail) {
+                     edump((*trace));
+                     throw *trace->except;
+                  }
+
+                  EOS_ASSERT( trx_receipts.size() > 0,
+                              block_validate_exception, "expected a receipt, block_num ${bn}, block_id ${id}, receipt ${e}",
+                              ("bn", b->block_num())("id", producer_block_id)("e", receipt)
+                     );
+                  EOS_ASSERT( trx_receipts.size() == num_pending_receipts + 1,
+                              block_validate_exception, "expected receipt was not added, block_num ${bn}, block_id ${id}, receipt ${e}",
+                              ("bn", b->block_num())("id", producer_block_id)("e", receipt)
+                     );
+                  const transaction_receipt_header& r = trx_receipts.back();
+                  EOS_ASSERT( r == static_cast<const transaction_receipt_header&>(receipt),
+                              block_validate_exception, "receipt does not match, ${lhs} != ${rhs}",
+                              ("lhs", r)("rhs", static_cast<const transaction_receipt_header&>(receipt)) );
                }
 
-               bool transaction_failed =  trace && trace->except;
-               bool transaction_can_fail = receipt.status == transaction_receipt_header::hard_fail && std::holds_alternative<transaction_id_type>(receipt.trx);
-               if( transaction_failed && !transaction_can_fail) {
-                  edump((*trace));
-                  throw *trace->except;
-               }
+               finalize_block();
 
-               EOS_ASSERT( trx_receipts.size() > 0,
-                           block_validate_exception, "expected a receipt, block_num ${bn}, block_id ${id}, receipt ${e}",
-                           ("bn", b->block_num())("id", producer_block_id)("e", receipt)
-                  );
-               EOS_ASSERT( trx_receipts.size() == num_pending_receipts + 1,
-                           block_validate_exception, "expected receipt was not added, block_num ${bn}, block_id ${id}, receipt ${e}",
-                           ("bn", b->block_num())("id", producer_block_id)("e", receipt)
-                  );
-               const transaction_receipt_header& r = trx_receipts.back();
-               EOS_ASSERT( r == static_cast<const transaction_receipt_header&>(receipt),
-                           block_validate_exception, "receipt does not match, ${lhs} != ${rhs}",
-                           ("lhs", r)("rhs", static_cast<const transaction_receipt_header&>(receipt)) );
-            }
+               auto& ab = std::get<assembled_block>(pending->_block_stage);
 
-            finalize_block();
+               if( producer_block_id != ab.id() ) {
+                  elog( "Validation block id does not match producer block id" );
 
-            auto& ab = std::get<assembled_block>(pending->_block_stage);
-
-            if( producer_block_id != ab.id() ) {
-               elog( "Validation block id does not match producer block id" );
-
-               // [greg todo] also call `report_block_header_diff in IF mode once we have a signed_block
-               ab.apply_dpos<void>([&](assembled_block::assembled_block_dpos& ab) { report_block_header_diff( *b, *ab.unsigned_block ); });
+                  // [greg todo] also call `report_block_header_diff in IF mode once we have a signed_block
+                  ab.apply_dpos<void>([&](assembled_block::assembled_block_dpos& ab) { report_block_header_diff( *b, *ab.unsigned_block ); });
             
-               // this implicitly asserts that all header fields (less the signature) are identical
-               EOS_ASSERT( producer_block_id == ab.id(), block_validate_exception, "Block ID does not match",
-                           ("producer_block_id", producer_block_id)("validator_block_id", ab.id()) );
-            }
+                  // this implicitly asserts that all header fields (less the signature) are identical
+                  EOS_ASSERT( producer_block_id == ab.id(), block_validate_exception, "Block ID does not match",
+                              ("producer_block_id", producer_block_id)("validator_block_id", ab.id()) );
+               }
 
-            if( !use_bsp_cached ) {
-               bsp->set_trxs_metas( ab.extract_trx_metas(), !skip_auth_checks );
-            }
-            // create completed_block with the existing block_state as we just verified it is the same as assembled_block
-            pending->_block_stage = completed_block{ bsp };
+               if( !use_bsp_cached ) {
+                  bsp->set_trxs_metas( ab.extract_trx_metas(), !skip_auth_checks );
+               }
+               // create completed_block with the existing block_state as we just verified it is the same as assembled_block
+               pending->_block_stage = completed_block{ bsp };
 
-            br = pending->_block_report; // copy before commit block destroys pending
-            commit_block(s);
-            br.total_time = fc::time_point::now() - start;
-         };
+               br = pending->_block_report; // copy before commit block destroys pending
+               commit_block(s);
+               br.total_time = fc::time_point::now() - start;
+            };
             block_data.apply_dpos<void>(do_the_work);
          }
          return;
