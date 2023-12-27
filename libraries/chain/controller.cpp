08730c6ec2c8a3bac042ec093b8da67079e095a2
greg7mdp <greg7mdp@gmail.com>
2023-12-27 16:07:35 -0500

    Add mermaid diagram

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -43,6 +43,34 @@
 #include <shared_mutex>
 #include <utility>
 
+/* ----------- Mermaid diagram -----------------------------------
+
+flowchart TD
+    pp[producer_plugin] --> D
+    A("`_replay()_`"):::fun --> B("`_replay_push_block()_`"):::fun
+    B --> E("`_maybe_switch_forks()_`"):::fun
+    C("`_init()_`"):::fun ---> E
+    C --> A
+    D("`_push_block()_`"):::fun ---> E
+    subgraph G["`**_apply_block()_**`"]
+       direction TB
+       start -- "stage = &Oslash;" --> sb
+       sb("`_start_block()_`"):::fun -- stage = building_block --> et
+       et["execute transactions" ] -- stage = building_block --> fb("`_finalize_block()_`"):::fun
+       fb -- stage = assembled block --> cb["add transaction metadata and create completed block"]
+       cb -- stage = completed block --> commit("`_commit_block()_ where we [maybe] add to fork_db and mark valid`"):::fun
+
+    end
+    B ----> start
+    E --> G
+    D --> F("`_log_irreversible()_`"):::fun
+    commit -- stage =  &Oslash; -->  F
+    F -- if in irreversible mode --> G
+
+    classDef fun fill:#f96
+
+------------------------------------------------------------------ */
+
 namespace eosio { namespace chain {
 
 using resource_limits::resource_limits_manager;
