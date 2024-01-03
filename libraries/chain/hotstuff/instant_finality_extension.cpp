cc9cbca92ebb105153693e656de2fe8eb102a27b
Lin Huang <lin.huang@eosnetwork.com>
2024-01-03 13:55:00 -0500

    remove unnecessary static_assert

--- libraries/chain/hotstuff/instant_finality_extension.cpp
+++ libraries/chain/hotstuff/instant_finality_extension.cpp
@@ -3,11 +3,11 @@
 
 namespace eosio::chain {
 
+#warning this file can be removed if no actual validation comes up after all design is complete
    void instant_finality_extension::reflector_init() {
       static_assert( fc::raw::has_feature_reflector_init_on_unpacked_reflected_types,
                      "instant_finality_extension expects FC to support reflector_init" );
       static_assert( extension_id() == 2, "instant_finality_extension extension id must be 2" );
-      static_assert( enforce_unique(), "instant_finality_extension must enforce uniqueness");
    }
 
 }  // eosio::chain
