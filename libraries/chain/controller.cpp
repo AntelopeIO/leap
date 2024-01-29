2b3f787016a424cc3583439df310795aa020e9e7
greg7mdp <greg7mdp@gmail.com>
2024-01-28 21:30:54 -0500

    Simplify redundent error message in `FC_ASSERT`, and other formatting changes.

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -3080,12 +3080,14 @@ struct controller_impl {
          // If there is no header extension, ensure the block does not have a QC and also the previous
          // block doesn't have a header extension either. Then return early.
          // ------------------------------------------------------------------------------------------
-         EOS_ASSERT( !qc_extension_present, block_validate_exception,
+         EOS_ASSERT( !qc_extension_present,
+                     block_validate_exception,
                      "Block #${b} includes a QC block extension, but doesn't have a finality header extension",
                      ("b", block_num) );
 
-         EOS_ASSERT( !prev_header_ext, block_validate_exception,
-                     "Block #${b} doesn't have a finality header extension even though its predecessor does. Block number: ${b}",
+         EOS_ASSERT( !prev_header_ext,
+                     block_validate_exception,
+                     "Block #${b} doesn't have a finality header extension even though its predecessor does.",
                      ("b", block_num) );
          return;
       }
@@ -3125,7 +3127,8 @@ struct controller_impl {
       if( qc_claim.last_qc_block_num == prev_qc_claim.last_qc_block_num ) {
          if( qc_claim.is_last_qc_strong == prev_qc_claim.is_last_qc_strong ) {
             // QC block extension is redundant
-            EOS_ASSERT( !qc_extension_present, block_validate_exception,
+            EOS_ASSERT( !qc_extension_present,
+                        block_validate_exception,
                         "Block #${b} should not provide a QC block extension since its QC claim is the same as the previous block's",
                         ("b", block_num) );
 
@@ -3135,31 +3138,36 @@ struct controller_impl {
          }
 
          // new claimed QC must be stronger than previous if the claimed block number is the same
-         EOS_ASSERT( qc_claim.is_last_qc_strong, block_validate_exception,
+         EOS_ASSERT( qc_claim.is_last_qc_strong,
+                     block_validate_exception,
                      "claimed QC (${s1}) must be stricter than previous block's (${s2}) if block number is the same. Block number: ${b}",
                      ("s1", qc_claim.is_last_qc_strong)("s2", prev_qc_claim.is_last_qc_strong)("b", block_num) );
       }
 
       // At this point, we are making a new claim in this block, so it better include a QC to justify this claim.
-      EOS_ASSERT( qc_extension_present, block_validate_exception,
+      EOS_ASSERT( qc_extension_present,
+                  block_validate_exception,
                   "Block #${b} is making a new finality claim, but doesn't include a qc to justify this claim", ("b", block_num) );
 
       const auto& qc_ext   = std::get<quorum_certificate_extension>(block_exts.lower_bound(qc_ext_id)->second);
       const auto& qc_proof = qc_ext.qc;
 
       // Check QC information in header extension and block extension match
-      EOS_ASSERT( qc_proof.block_num == qc_claim.last_qc_block_num,  block_validate_exception,
+      EOS_ASSERT( qc_proof.block_num == qc_claim.last_qc_block_num,
+                  block_validate_exception,
                   "Block #${b}: Mismatch between qc.block_num (${n1}) in block extension and last_qc_block_num (${n2}) in header extension",
                   ("n1", qc_proof.block_num)("n2", qc_claim.last_qc_block_num)("b", block_num) );
 
       // Verify claimed strictness is the same as in proof
-      EOS_ASSERT( qc_proof.qc.is_strong() == qc_claim.is_last_qc_strong, block_validate_exception,
+      EOS_ASSERT( qc_proof.qc.is_strong() == qc_claim.is_last_qc_strong,
+                  block_validate_exception,
                   "QC is_strong (${s1}) in block extension does not match is_last_qc_strong (${s2}) in header extension. Block number: ${b}",
                   ("s1", qc_proof.qc.is_strong())("s2", qc_claim.is_last_qc_strong)("b", block_num) );
 
       // find the claimed block's block state on branch of id
       auto bsp = fork_db_fetch_bsp_by_num( prev.id, qc_claim.last_qc_block_num );
-      EOS_ASSERT( bsp, block_validate_exception,
+      EOS_ASSERT( bsp,
+                  block_validate_exception,
                   "Block state was not found in forkdb for last_qc_block_num ${q}. Block number: ${b}",
                   ("q", qc_claim.last_qc_block_num)("b", block_num) );
 
@@ -3175,7 +3183,8 @@ struct controller_impl {
       verify_qc_claim(id, b, prev);
 
       auto trx_mroot = calculate_trx_merkle( b->transactions, true );
-      EOS_ASSERT( b->transaction_mroot == trx_mroot, block_validate_exception,
+      EOS_ASSERT( b->transaction_mroot == trx_mroot,
+                  block_validate_exception,
                   "invalid block transaction merkle root ${b} != ${c}", ("b", b->transaction_mroot)("c", trx_mroot) );
 
       const bool skip_validate_signee = false;
