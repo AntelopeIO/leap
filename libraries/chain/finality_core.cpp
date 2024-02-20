1e7e5c019d2c4d26ec9a917fbf0c21f26e0e5a09
Lin Huang <lin.huang@eosnetwork.com>
2024-02-20 11:40:50 -0500

    update finality_core

--- libraries/chain/finality_core.cpp
+++ libraries/chain/finality_core.cpp
@@ -1,96 +1,38 @@
-using block_num_type  = uint32_t;
-using block_time_type = chain::block_timestamp_type;
+#include <eosio/chain/finality_core.hpp>
+#include <eosio/chain/block_header.hpp>
 
-struct block_ref
-{
-   block_id_type    block_id;
-   block_time_type  timestamp;
-
-   block_num_type block_num() const; // Extract from block_id.
-};
-
-struct qc_link
-{
-   block_num_type  source_block_num;
-   block_num_type  target_block_num; // Must be less than or equal to source_block_num (only equal for genesis block).
-   bool            is_link_strong;
-};
-
-struct qc_claim
-{
-   block_num_type  block_num;
-   bool            is_strong_qc;
-};
-
-struct core
-{
-   std::vector<qc_link>    links; // Captures all relevant links sorted in order of ascending source_block_num.
-   std::vector<block_ref>  refs; // Covers ancestor blocks with block numbers greater than or equal to last_final_block_num.
-                                 // Sorted in order of ascending block_num.
-   block_num_type          final_on_strong_qc_block_num;
-
-   // Invariants:
-   // 1. links.empty() == false
-   // 2. last_final_block_num() <= final_on_strong_qc_block_num <= latest_qc_claim().block_num
-   // 3. If refs.empty() == true, then (links.size() == 1) and 
-   //                                  (links.back().target_block_num == links.back().source_block_num == final_on_strong_qc_block_num == last_final_block_num())
-   // 4. If refs.empty() == false, then refs.front().block_num() == links.front().target_block_num == last_final_block_num()
-   // 5. If refs.empty() == false, then refs.back().block_num() + 1 == links.back().source_block_num == current_block_num()
-   // 6. If refs.size() > 1, then:
-   //       For i = 0 to refs.size() - 2: 
-   //          (refs[i].block_num() + 1 == refs[i+1].block_num()) and (refs[i].timestamp < refs[i+1].timestamp)
-   // 7. If links.size() > 1, then:
-   //       For i = 0 to links.size() - 2:
-   //          (links[i].source_block_num + 1 == links[i+1].source_block_num) and (links[i].target_block_num <= links[i+1].target_block_num)
-   // 8. current_block_num() - last_final_block_num() == refs.size() (always implied by invariants 3 to 6)
-   // 9. current_block_num() - links.front().source_block_num == links.size() - 1 (always implied by invariants 1 and 7)
-
-   static core create_core_for_genesis_block(block_num_type block_num)
-   {
-      return {
-         .links                        = {
-            qc_link{
-               .source_block_num = block_num,
-               .target_block_num = block_num,
-               .is_strong_qc     = false,
-            },
-         },
-         .refs                         = {},
-         .final_on_strong_qc_block_num = block_num,
-         .last_final_block_num         = block_num,
-      };
-
-      // Invariants 1 to 7 can be easily verified to be satisfied for the returned core. 
-      // (And so, remaining invariants are also automatically satisfied.)
+namespace eosio::chain {
+   block_num_type block_ref::block_num() const {
+      return block_header::num_from_id(block_id);
    }
 
-   block_num_type current_block_num() const
+   block_num_type finality_core::current_block_num() const
    {
       assert(!links.empty()); // Satisfied by invariant 1.
 
       return links.back().source_block_num;
    }
 
-   block_num_type last_final_block_num() const
+   block_num_type finality_core::last_final_block_num() const
    {
       assert(!links.empty()); // Satisfied by invariant 1.
 
       return links.front().target_block_num;
    }
 
-
-   qc_claim latest_qc_claim() const
+   qc_claim finality_core::latest_qc_claim() const
    {
       assert(!links.empty()); // Satisfied by invariant 1.
 
       return qc_claim{.block_num = links.back().target_block_num, .is_strong_qc = links.back().is_link_strong};
    }
+
    /**
     *  @pre last_final_block_num() <= block_num < current_block_num()
     *
     *  @post returned block_ref has block_num() == block_num
     */
-   const block_ref& get_block_reference(block_num_type block_num) const
+   const block_ref& finality_core::get_block_reference(block_num_type block_num) const
    {
       assert(last_final_block_num() <= block_num); // Satisfied by precondition.
       assert(block_num < current_block_num()); // Satisfied by precondition.
@@ -105,7 +47,7 @@ struct core
 
       assert(ref_index < refs.size()); // Satisfied by justification above.
 
-      return tail[ref_index];
+      return refs[ref_index];
       // By invariants 4 and 6, tail[ref_index].block_num() == block_num, which satisfies the post-condition.
    }
 
@@ -114,7 +56,7 @@ struct core
     *
     *  @post returned qc_link has source_block_num == block_num
     */
-   const qc_link& get_qc_link_from(block_num_type block_num) const
+   const qc_link& finality_core::get_qc_link_from(block_num_type block_num) const
    {
       assert(!links.empty()); // Satisfied by invariant 1.
 
@@ -126,7 +68,7 @@ struct core
       // By the precondition, 0 <= link_index <= (current_block_num() - links.front().source_block_num).
       // Then, by invariant 9, 0 <= link_index <= links.size() - 1
 
-      assert(link_index < link.size()); // Satisfied by justification above.
+      assert(link_index < links.size()); // Satisfied by justification above.
 
       return links[link_index];
       // By invariants 7, links[link_index].source_block_num == block_num, which satisfies the post-condition.
@@ -136,25 +78,28 @@ struct core
     *  @pre current_block.block_num() == this->current_block_num()
     *  @pre If this->refs.empty() == false, then current_block is the block after the one referenced by this->refs.back()
     *  @pre this->latest_qc_claim().block_num <= most_recent_ancestor_with_qc.block_num <= this->current_block_num()
-    *  @pre this->latest_qc_claim() <= most_recent_ancestor_with_qc
+    *  @pre this->latest_qc_claim() <= most_recent_ancestor_with_qc ( (this->latest_qc_claim().block_num == most_recent_ancestor_with_qc.block_num) && most_recent_ancestor_with_qc.is_strong_qc ). When block_num is the same, most_recent_ancestor_with_qc must be stronger than latest_qc_claim()
     *
     *  @post returned core has current_block_num() == this->current_block_num() + 1
     *  @post returned core has latest_qc_claim() == most_recent_ancestor_with_qc
     *  @post returned core has final_on_strong_qc_block_num >= this->final_on_strong_qc_block_num
     *  @post returned core has last_final_block_num() >= this->last_final_block_num()
     */
-   core next(const block_ref& current_block, const qc_claim& most_recent_ancestor_with_qc) const
+   finality_core finality_core::next(const block_ref& current_block, const qc_claim& most_recent_ancestor_with_qc) const
    {
       assert(current_block.block_num() == current_block_num()); // Satisfied by precondition 1.
       
-      assert(refs.empty() || (refs.back().timestamp < current_block.timestamp)); // Satisfied by precondition 2.
+      assert(refs.empty() || (refs.back().timestamp <= current_block.timestamp)); // Satisfied by precondition 2.
       assert(refs.empty() || (refs.back().block_num() + 1 == current_block.block_num())); // Satisfied by precondition 2.
 
       assert(most_recent_ancestor_with_qc.block_num <= current_block_num()); // Satisfied by precondition 3.
 
-      assert(latest_qc_claim() <= most_recent_ancestor_with_qc) // Satisfied by precondition 4.
+      assert(refs.empty() ||
+             ((latest_qc_claim().block_num < most_recent_ancestor_with_qc.block_num) ||
+              ((latest_qc_claim().block_num == most_recent_ancestor_with_qc.block_num) &&
+               (!latest_qc_claim().is_strong_qc || most_recent_ancestor_with_qc.is_strong_qc)))); // Satisfied by precondition 4.
 
-      core next_core;
+      finality_core next_core;
 
       auto new_block_nums = [&]() -> std::pair<block_num_type, block_num_type> 
          {
@@ -197,12 +142,11 @@ struct core
          };
 
       const auto [new_last_final_block_num, new_final_on_strong_qc_block_num] = new_block_nums();
-
       assert(new_last_final_block_num <= new_final_on_strong_qc_block_num); // Satisfied by justification in new_block_nums.
       assert(new_final_on_strong_qc_block_num <= most_recent_ancestor_with_qc.block_num); // Satisfied by justification in new_block_nums.
 
       assert(final_on_strong_qc_block_num <= new_final_on_strong_qc_block_num); // Satisfied by justifications in new_block_nums.
-      assert(last_final_block_num <= new_last_final_block_num); // Satisfied by justifications in new_block_nums.
+      assert(last_final_block_num() <= new_last_final_block_num); // Satisfied by justifications in new_block_nums.
 
       next_core.final_on_strong_qc_block_num = new_final_on_strong_qc_block_num;
       // Post-condition 3 is satisfied, assuming next_core will be returned without further modifications to next_core.final_on_strong_qc_block_num.
@@ -213,13 +157,16 @@ struct core
       {
          size_t links_index = 0; // Default to no garbage collection (if last_final_block_num does not change).
 
-         if (last_final_block_num < next_core.last_final_block_num) {
+         if (last_final_block_num() < new_last_final_block_num) {
             // new_blocks_nums found the new_last_final_block_num from a link that had a source_block_num
             // equal to new_final_on_strong_qc_block_num.
             // The index within links was (new_final_on_strong_qc_block_num - last_final_block_num).
             // All prior links can be garbage collected.
 
-            links_index = new_final_on_strong_qc_block_num - last_final_block_num;
+            links_index = new_last_final_block_num - last_final_block_num();
+            while ( links_index < links.size() && links[links_index].target_block_num == last_final_block_num() ) {
+               ++links_index;
+            }
 
             assert(links_index < links.size()); // Satisfied by justification in this->get_qc_link_from(next_core.final_on_strong_qc_block_num).
          }
@@ -250,7 +197,7 @@ struct core
 
       // Setup next_core.refs by garbage collecting unnecessary block references in the refs and then adding the new block reference.
       {
-         const size_t refs_index = next_core.last_final_block_num - last_final_block_num;
+         const size_t refs_index = next_core.last_final_block_num() - last_final_block_num();
 
          // Using the justifications in new_block_nums, 0 <= ref_index <= (current_block_num() - last_final_block_num).
          // If refs.empty() == true, then by invariant 3, current_block_num() == last_final_block_num, and therefore ref_index == 0.
@@ -261,15 +208,15 @@ struct core
          assert(!refs.empty() || (refs_index == 0)); // Satisfied by justification above.
          assert(refs.empty() || (refs_index < refs.size())); // Satisfied by justification above.
 
-         next_core.refs.reserve(refs.size() - ref_index + 1);
+         next_core.refs.reserve(refs.size() - refs_index + 1);
 
          // Garbage collect unnecessary block references
          std::copy(refs.cbegin() + refs_index, refs.cend(), std::back_inserter(next_core.refs));
 
-         assert(refs.empty() || (refs.front().block_num() == new_last_final_block_num)); // Satisfied by choice of refs_index.
+         assert(refs.empty() || (next_core.refs.front().block_num() == new_last_final_block_num)); // Satisfied by choice of refs_index.
 
          // Add new block reference
-         next_core.tail.emplace_back(current_block);
+         next_core.refs.emplace_back(current_block);
 
          // Invariant 3 is trivially satisfied for next_core because next_core.refs.empty() == false.
 
@@ -285,7 +232,7 @@ struct core
          // If this->refs.empty() == false, then adding the current_block to the end does not change the fact that
          // refs.front().block_num() is still equal to new_last_final_block_num.
 
-         assert(refs.front().block_num() == new_last_final_block_num)); // Satisfied by justification above.
+         assert(refs.empty() || (next_core.refs.front().block_num() == new_last_final_block_num)); // Satisfied by justification above.
 
          // Because it was also already shown earlier that links.front().target_block_num == new_last_final_block_num,
          // then the justification above satisfies the remaining equalities needed to satisfy invariant 4 for next_core.
@@ -298,8 +245,9 @@ struct core
       // Invariants 1 to 7 were verified to be satisfied for the current value of next_core at various points above. 
       // (And so, the remaining invariants for next_core are also automatically satisfied.)
    }
-};
+} /// eosio::chain
 
+#if 0
 //--------------
 
 // Note: The code below is not fully fleshed out and is not fully compatible with the way the Leap implementation will actually handle this.
@@ -411,3 +359,4 @@ struct minimal_state
       return next_state;
    }
 };
+#endif
