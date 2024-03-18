dce42d13eb7ca217e6814ec3249e54791b01f76f
greg7mdp <greg7mdp@gmail.com>
2024-03-18 13:53:11 -0400

    Update comment.

--- libraries/chain/include/eosio/chain/incremental_merkle.hpp
+++ libraries/chain/include/eosio/chain/incremental_merkle.hpp
@@ -18,7 +18,7 @@ namespace detail {
 constexpr uint64_t calculate_max_depth(uint64_t node_count) {
    if (node_count == 0)
       return 0;
-   // following is non-floating point equivalent to `std::ceil(std::log2(node_count)) + 1)
+   // following is non-floating point equivalent to `std::ceil(std::log2(node_count)) + 1)` (and about 9x faster)
    return 8*sizeof(node_count) - std::countl_zero(std::bit_ceil(node_count));
 }
 
