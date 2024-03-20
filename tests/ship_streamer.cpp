b1b964279dcec1c4dde1bd8af45a53b6d3feb5eb
Lin Huang <lin.huang@eosnetwork.com>
2024-03-20 15:42:02 -0400

    remove leftover debugging statements

--- tests/ship_streamer.cpp
+++ tests/ship_streamer.cpp
@@ -122,14 +122,10 @@ int main(int argc, char* argv[]) {
          }
          request_writer.EndObject();
       request_writer.EndArray();
-      std::cerr << "01\n";
 
       stream.binary(true);
-      std::cerr << "02\n";
       stream.write(boost::asio::buffer(request_type.json_to_bin(request_sb.GetString(), [](){})));
-      std::cerr << "10\n";
       stream.read_message_max(0);
-      std::cerr << "11\n";
 
       // Each block_num can have multiple block_ids since forks are possible
       //       block_num,         block_id
@@ -138,14 +134,10 @@ int main(int argc, char* argv[]) {
       for(;;) {
          boost::beast::flat_buffer buffer;
          stream.read(buffer);
-         std::cerr << "1\n";
 
          eosio::input_stream is((const char*)buffer.data().data(), buffer.data().size());
-         std::cerr << "2\n";
          rapidjson::Document result_document;
-         //std::cerr << result_type.bin_to_json(is).c_str() << std::endl;
          result_document.Parse(result_type.bin_to_json(is).c_str());
-         std::cerr << "3\n";
 
          eosio::check(!result_document.HasParseError(),                                      "Failed to parse result JSON from abieos");
          eosio::check(result_document.IsArray(),                                             "result should have been an array (variant) but it's not");
