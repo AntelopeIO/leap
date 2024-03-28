bf04b0a266b69eef972d3bed4c67a609ac324928
Kevin Heifner <heifnerk@objectcomputing.com>
2024-03-28 07:58:35 -0500

    GH-2313 Fix spelling

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -481,7 +481,7 @@ struct building_block {
          if (!parent.proposer_policies.empty()) {
             block_timestamp_type active_time = detail::get_next_next_round_block_time(timestamp);
             if (auto itr = parent.proposer_policies.find(active_time); itr != parent.proposer_policies.cend()) {
-               return itr->second->proposer_schedule.version; // will replace so retrun same version
+               return itr->second->proposer_schedule.version; // will replace so return same version
             }
             return (--parent.proposer_policies.end())->second->proposer_schedule.version + 1;
          }
