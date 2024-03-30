c300efe48e5b4c65bbe8461959707303251ecda1
Kevin Heifner <heifnerk@objectcomputing.com>
2024-03-30 13:25:01 -0500

    GH-2364 Verify needed trx processed before continuing

--- tests/nodeos_retry_transaction_test.py
+++ tests/nodeos_retry_transaction_test.py
@@ -135,9 +135,10 @@ try:
     startTime = time.perf_counter()
     Print("Create new accounts via %s" % (cluster.eosioAccount.name))
     for account in accounts:
-        trans = node.createInitializeAccount(account, cluster.eosioAccount, stakedDeposit=0, waitForTransBlock=(account == accounts[-1]), stakeNet=1000, stakeCPU=1000, buyRAM=1000, exitOnError=True)
+        trans = node.createInitializeAccount(account, cluster.eosioAccount, stakedDeposit=0, waitForTransBlock=False, stakeNet=1000, stakeCPU=1000, buyRAM=1000, exitOnError=True)
         checkTransIds.append(Node.getTransId(trans))
 
+    node.waitForTransactionsInBlock(checkTransIds)
     nextTime = time.perf_counter()
     Print("Create new accounts took %s sec" % (nextTime - startTime))
     startTime = nextTime
@@ -146,18 +147,20 @@ try:
     for account in accounts:
         transferAmount="1000.0000 {0}".format(CORE_SYMBOL)
         Print("Transfer funds %s from account %s to %s" % (transferAmount, cluster.eosioAccount.name, account.name))
-        trans = node.transferFunds(cluster.eosioAccount, account, transferAmount, "test transfer", waitForTransBlock=(account == accounts[-1]), reportStatus=False)
+        trans = node.transferFunds(cluster.eosioAccount, account, transferAmount, "test transfer", waitForTransBlock=False, reportStatus=False)
         checkTransIds.append(Node.getTransId(trans))
 
+    node.waitForTransactionsInBlock(checkTransIds)
     nextTime = time.perf_counter()
     Print("Transfer funds took %s sec" % (nextTime - startTime))
     startTime = nextTime
 
     Print("Delegate Bandwidth to new accounts")
     for account in accounts:
-        trans=node.delegatebw(account, 200.0000, 200.0000, waitForTransBlock=(account == accounts[-1]), exitOnError=True, reportStatus=False)
+        trans=node.delegatebw(account, 200.0000, 200.0000, waitForTransBlock=False, exitOnError=True, reportStatus=False)
         checkTransIds.append(Node.getTransId(trans))
 
+    node.waitForTransactionsInBlock(checkTransIds)
     nextTime = time.perf_counter()
     Print("Delegate Bandwidth took %s sec" % (nextTime - startTime))
     startTime = nextTime
